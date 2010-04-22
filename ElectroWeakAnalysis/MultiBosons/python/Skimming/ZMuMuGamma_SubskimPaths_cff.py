35e0f4e6006bd3ba5b1efbf3f7b978876d2fcc0c
Jan Veverka <jan.veverka@gmail.com>
2010-04-22 09:20:26 +0000

    "renaming and cleaning up unused files"

--- ElectroWeakAnalysis/MultiBosons/python/Skimming/ZMuMuGamma_SubskimPaths_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from ElectroWeakAnalysis.MultiBosons.Skimming.mumuGammas_cfi import *
-from ElectroWeakAnalysis.MultiBosons.Skimming.mumuGammaFilter_cfi import *
-
-mumuGammaPath = cms.Path(
-  mumuGammas *
-  mumuGammaFilter
-)
