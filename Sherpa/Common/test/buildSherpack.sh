239d3763cbebff2cdca322e7c023215d8a5a10a7
Jan Veverka <jan.veverka@gmail.com>
2010-05-26 12:36:42 +0000

    Updated Zg*_0j* to Sherpa 1.2.1

--- Sherpa/Common/test/buildSherpack.sh
+++ Sherpa/Common/test/buildSherpack.sh
@@ -5,7 +5,7 @@ CMSSW_RELEASE=CMSSW_3_5_8
 ##+ 2 - Sherpa 1.2.1 with CMSSW_3_5_8 in May 2010
 PROJECT_VERSION=2
 ## UserCode/JanVeverka/Sherpa release tag
-TAG=V01-01-00
+TAG=V01-01-01
 
 ## setup CMSSW release area
 scramv1 project CMSSW $CMSSW_RELEASE
@@ -34,19 +34,34 @@ tar -czf sherpa_${PROCESS_NAME}_cards.tgz Run.dat Analysis.dat
 ## run step 2
 (time ./PrepareSherpaLibs.sh -i ./ -p $PROCESS_NAME -a Sherpa/$PROCESS_NAME -m PROD) >& step2.out
 
-## get unique output name
-CASTOR_PATH=$CASTOR_HOME/mc/Spring10/Sherpa_v${PROJECT_VERSION}
-CASTOR_OUTPUT_PATH=$CASTOR_PATH/sherpacks
-# SHERPACK_VERSION=0
-# NAME=sherpa_${PROCESS_NAME}_v${SHERPACK_VERSION}_MASTER.tgz
-# while nsls $CASTOR_OUTPUT_PATH | grep -q $NAME; do
-# 	((SHERPACK_VERSION++))
-# 	NAME=sherpa_${PROCESS_NAME}_v${SHERPACK_VERSION}_MASTER.tgz
-# done
+#### Store the sherpack
+SHERPACK_DIR=sherpacks
+SHERPACK=sherpa_${PROCESS_NAME}_MASTER.tgz
 
-## Store output on CASTOR
-# rfcp sherpa_${PROCESS_NAME}_MASTER.tgz $CASTOR_OUTPUT_PATH/$NAME
-rfcp sherpa_${PROCESS_NAME}_MASTER.tgz $CASTOR_OUTPUT_PATH
+## On the T3
+T3_PATH=/mnt/hadoop/user/veverka/mc/Spring10/Sherpa_v2
+SHERPACK_T3_DIR=$T3_PATH/$SHERPACK_DIR
+ssh t3-susy.ultralight.org \
+  if [[ ! -d $SHERPACK_T3_DIR]]; then \
+    mkdir -p $SHERPACK_T3_DIR; \
+  fi
+scp $SHERPACK t3-susy.ultralight.org:$SHERPACK_T3_DIR
+
+## On CASTOR
+CASTOR_PATH=$CASTOR_HOME/mc/Spring10/Sherpa_v2
+SHERPACK_CASTOR_DIR=$CASTOR_PATH/$SHERPACK_DIR
+if ! ls $SHERPACK_CASTOR_DIR >& /dev/null; then
+  rfmkdir -p $SHERPACK_CASTOR_DIR
+fi
+rfcp $SHERPACK $SHERPACK_CASTOR_DIR
+
+## On the AFS
+AFS_PATH=/afs/cern.ch/cms/cit/veverka/data/mc/Spring10/Sherpa_v2
+SHERPACK_AFS_DIR=$AFS_PATH/$SHERPACK_DIR
+if [[ ! -d $SHERPACK_AFS_DIR ]]; then
+  mkdir -p $SHERPACK_CASTOR_DIR
+fi
+cp $SHERPACK $SHERPACK_AFS_DIR
 
 ## clean up
 cd $CMSSW_BASE/..
