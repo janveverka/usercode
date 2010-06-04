65c2a4f7de1ecde96d826b0cacf16dd58353e67e
Jan Veverka <jan.veverka@gmail.com>
2010-06-04 13:41:08 +0000

    This commit was manufactured by cvs2git to create tag 'V01-01-05'.
    Sprout from master 2010-06-04 13:41:07 UTC Jan Veverka <jan.veverka@gmail.com> 'Updated run card tag'
    Delete:
        ElectroWeakAnalysis/MultiBosons/python/Skimming/mumuGammaFilter_cfi.py
        ElectroWeakAnalysis/MultiBosons/python/Skimming/mumuGammaOneTrackFilter_cfi.py
        ElectroWeakAnalysis/MultiBosons/python/Skimming/mumuGammasOneTrack_cfi.py
        ElectroWeakAnalysis/MultiBosons/python/Skimming/mumuGammas_cfi.py
        ElectroWeakAnalysis/MultiBosons/python/Skimming/vgEventContent.py
        ElectroWeakAnalysis/MultiBosons/python/Skimming/zMuMuGammaSubskim_cfi.py
        ElectroWeakAnalysis/MultiBosons/python/Skimming/zMuMuGamma_SubskimSequences_cff.py
        ElectroWeakAnalysis/MultiBosons/python/analysis_cfi.py
        ElectroWeakAnalysis/MultiBosons/test/Skimming/zMuMuGammaSubskim.py
        ElectroWeakAnalysis/MultiBosons/test/patTuple_MuMuGamma_cfg.py
        FnalTest/empty.txt
        JPsi/MuMu/doc/README
        JPsi/MuMu/python/makeJPsiGenHistos_cfi.py
        JPsi/MuMu/python/testJPsiSkim_cfi.py
        JPsi/MuMu/test/empty.txt
        JPsi/MuMu/test/makeJPsiGenHistos_cfg.py
        JPsi/MuMu/test/plotJpsiMass.py
        JPsi/MuMu/test/testJPsiSkim.py

--- ElectroWeakAnalysis/MultiBosons/python/Skimming/zMuMuGamma_SubskimSequences_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from ElectroWeakAnalysis.MultiBosons.Skimming.mumuGammas_cfi import *
-from ElectroWeakAnalysis.MultiBosons.Skimming.mumuGammaFilter_cfi import *
-
-mumuGammaSequence = cms.Sequence(
-  mumuGammas *
-  mumuGammaFilter
-)
