bd5d5bed85c95b4621a5c8809edb3cd8749c373e
Jan Veverka <jan.veverka@gmail.com>
2011-04-07 02:05:59 +0000

    A more elaborate example dumping various photon quantities

---
+++ Misc/TreeMaker/python/photonTreeMaker_cfi.py
@@ -0,0 +1,62 @@
+import FWCore.ParameterSet.Config as cms
+
+MuMuGammaTree = cms.EDAnalyzer("TreeMaker",
+  name = cms.untracked.string("mmg"),
+  title = cms.untracked.string("mmg TreeMaker"),
+  src = cms.InputTag("cleanPatPhotonsTriggerMatch"),
+  prefix = cms.untracked.string("cand"),
+  sizeName = cms.untracked.string("nPhotons"),
+  variables = cms.VPSet()
+)
+
+branches = """Pt                pt
+              Eta               eta
+              ScEta             superCluster.eta
+              Phi               phi
+              EcalIso           ecalIso
+              HcalIso           hcalIso
+              TrackIso          trackIso
+              SigmaIetaIeta     sigmaIetaIeta
+              HadronicOverEm    hadronicOverEm
+              HasPixelSeed      hasPixelSeed
+              SeedRecoFlag      userInt("photonUserData:seedRecoFlag")
+              SeedSeverityLevel userInt("photonUserData:seedSeverityLevel")
+              MaxEnergyXtal     maxEnergyXtal
+              E3x3              e3x3
+              SeedSwissCross    userInt("photonUserData:seedSwissCross")
+              SeedE1OverE9      userInt("photonUserData:seedE1OverE9")
+              R9                r9 
+              ESC               superCluster.energy 
+              ESCRaw            superCluster.rawEnergy
+              E5x5              e5x5""".split("\n")
+
+genBranches =  """GenMatchPdgId     genParticle.pdgId
+                  GenMatchStatus    genParticle.status
+                  GenMatchMomPdgId  genParticle.mother(0).pdgId
+                  GenMatchMomStatus genParticle.mother(0).status
+                  GenPt             genParticle.pt
+                  GenEta            genParticle.eta
+                  GenPhi            genParticle.phi""".split("\n")
+
+for line in  branches:
+    tag, var = line.split()
+    MuMuGammaTree.variables.append(
+        cms.PSet(
+            tag = cms.untracked.string(tag),
+            quantity = cms.untracked.string(var)
+        )
+    )
+
+for line in genBranches: 
+    tag, var = line.split()
+    MuMuGammaTree.variables.append(
+        cms.PSet(
+            tag = cms.untracked.string(tag),
+            quantity = cms.untracked.PSet(
+                ifCondition = cms.untracked.string("genParticlesSize > 0"),
+                thenQuantity = cms.untracked.string(var),
+                elseQuantity = cms.untracked.string("0")
+            )
+        )
+    )
+
---
+++ Misc/TreeMaker/test/photonTreeMaker_cfg.py
@@ -0,0 +1,55 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+# setup 'analysis'  options
+options = VarParsing.VarParsing ('analysis')
+
+## default options
+options.inputFiles = [
+    "file:/mnt/hadoop/user/veverka/DimuonVGammaSkim_v3/Mu/Run2010A-Nov4ReReco_v1-DimuonVGammaSkim_v3/f81f6ec801d687f509099b4fa4a3c90c/"
+    + f for f in """
+        merge_10_1_xJE.root  merge_13_1_ndW.root  merge_1_1_f6L.root
+        merge_11_1_BKq.root  merge_14_1_wuM.root  merge_2_1_1Me.root
+        merge_12_1_8F1.root  merge_15_1_PDX.root  merge_3_1_RXC.root
+        merge_4_1_unc.root  merge_7_1_s8z.root
+        merge_5_1_aPx.root  merge_8_1_gdx.root
+        merge_6_1_ran.root  merge_9_1_RlG.root
+        """.split()
+]
+
+options.outputFile = "MuMuGammaTree.root"
+
+## get and parse the command line arguments
+options.parseArguments()
+
+process = cms.Process("TREE")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+## Enable LogDebug for MuMuGammaTree module
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+process.MessageLogger.debugModules = ["tree"]
+#process.MessageLogger.cerr.threshold = "DEBUG"
+
+process.maxEvents = cms.untracked.PSet(
+  input = cms.untracked.int32(options.maxEvents)
+)
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring() + options.inputFiles
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string(options.outputFile)
+)
+
+process.load("Misc.TreeMaker.photonTreeMaker_cfi")
+
+process.options = cms.untracked.PSet(
+    SkipEvent = cms.untracked.vstring('ProductNotFound')
+)
+
+process.p = cms.Path(process.MuMuGammaTree)
+
+
+if __name__ == "__main__": import user
