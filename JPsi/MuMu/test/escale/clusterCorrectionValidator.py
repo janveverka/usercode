ba2e83aba5af5e612b0b11cee0ee6814e7eb5055
Jan Veverka <jan.veverka@gmail.com>
2011-07-13 15:28:26 +0000

    initial commit

---
+++ JPsi/MuMu/test/escale/clusterCorrectionValidator.py
@@ -0,0 +1,141 @@
+import os
+import JPsi.MuMu.common.dataset as dataset
+import JPsi.MuMu.common.energyScaleChains as esChains
+import JPsi.MuMu.common.clusterCorrections as clusterCorrs
+
+from JPsi.MuMu.common.basicRoot import *
+from JPsi.MuMu.common.roofit import *
+
+ws1 = RooWorkspace( 'ws1', 'mmg energy scale' )
+
+
+x = RooRealVar( 's', '100 * (1/kRatio - 1)', -50, 50, '%' )
+x.setBins(50)
+
+w = RooRealVar( 'w', '1', 0, 99 )
+
+xw = RooArgSet(x, w)
+ws1.Import(xw)
+# label = 'EB_highR9'
+# label = 'EE_highR9'
+# label = 'EB_lowR9'
+# label = 'EE_lowR9'
+label = 'EB'
+# label = 'EE'
+
+## Get data
+selection = [
+    'abs(1/kRatio - 1) < 0.5',
+    'abs(mmgMass-91.2) < 4',
+]
+
+if label == 'EB_highR9':
+    selection.extend( ['phoR9 > 0.94', 'phoIsEB'] )
+elif label == 'EB_lowR9':
+    selection.extend( ['phoR9 < 0.94', 'phoIsEB'] )
+elif label == 'EE_highR9':
+    selection.extend( ['phoR9 > 0.95', '!phoIsEB'] )
+elif label == 'EE_lowR9':
+    selection.extend( ['phoR9 < 0.95', '!phoIsEB'] )
+elif label == 'EB':
+    selection.extend( ['phoIsEB'] )
+    highR9Cut = '(phoR9 > 0.94)'
+elif label == 'EE':
+    selection.extend( ['!phoIsEB'] )
+    highR9Cut = '(phoR9 > 0.95)'
+else:
+    raise RuntimeError, "Unknown label `%s'" % label
+
+model = ws1.factory("""CBShape::crystalBall( s,
+                                             #Deltas[0, -50, 50],
+                                             #sigma[20, 0.001, 100],
+                                             #alpha[-1.5, -10, 0],
+                                             n[1.5, 0.1, 10] )""")
+
+_chains = esChains.getChains('v7')
+
+for ch in _chains.values():
+    ch.SetAlias( 'phoE', 'phoPt * cosh(phoEta)' )
+    ch.SetAlias( 'brem', 'scPhiWidth / scEtaWidth' )
+    ch.SetAlias( 'rawE', 'scRawE + preshowerE' )
+    ch.SetAlias( 'corrE', 'phoCrackCorr * corrE(rawE, scEta, brem)' )
+    ch.SetAlias( 'newCorrE', 'phoCrackCorr * newCorrE(rawE, scEta, brem)' )
+
+
+x.SetTitle( '100 * (1/kRatio - 1)' )
+# x.SetTitle( '100 * ( newCorrE / (phoE*kRatio) - 1)' )
+data1 = dataset.get( tree = _chains['data'],
+                     variable = x, weight = w, cuts = selection + [highR9Cut] )
+data1.SetName('data1')
+ws1.Import(data1)
+
+# x.SetTitle( '100 * (1/kRatio - 1)' )
+x.SetTitle( '100 * ( newCorrE / (phoE*kRatio) - 1)' )
+data2 = dataset.get( tree = _chains['data'],
+                     variable = x, weight = w, cuts = selection + ['!' + highR9Cut] )
+data2.SetName('data2')
+ws1.Import(data2)
+
+
+## Define observables and parameters
+observables = RooArgSet(x)
+parameters = model.getParameters(observables)
+ws1.defineSet("parameters", parameters)
+ws1.saveSnapshot("initial", parameters, True)
+
+## Fit data1
+fitResult1 = model.fitTo( data1, Save(), SumW2Error(kTRUE), PrintLevel(-1) )
+ws1.saveSnapshot( 'data1', parameters, True )
+
+f1 = 1 + ws1.var('#Deltas').getVal() / 100
+f1err = ws1.var('#Deltas').getError() / 100
+
+x.SetTitle('s = E_{RECO}/E_{KIN} - 1')
+x.setBins(40)
+plot = x.frame( Range(-30, 50) )
+data1.SetTitle('data1')
+data1.plotOn(plot)
+model.plotOn(plot)
+model.paramOn( plot,
+               Format('NEU', AutoPrecision(2) ),
+               Parameters(parameters),
+               Layout(.57, 0.92, 0.92) )
+
+c1 = TCanvas()
+c1.Divide(2,1)
+c1.cd(1)
+plot.Draw()
+
+## Fit MC
+fitResult2 = model.fitTo( data2, Save(), SumW2Error(kTRUE), PrintLevel(-1) )
+ws1.saveSnapshot( 'MC', parameters, True )
+
+f2 = 1 + ws1.var('#Deltas').getVal() / 100
+f2err = ws1.var('#Deltas').getError() / 100
+
+x.setBins(40)
+plot = x.frame( Range(-30, 50) )
+data2.SetTitle('MC')
+data2.plotOn(plot)
+model.plotOn(plot)
+model.paramOn( plot,
+               Format('NEU', AutoPrecision(2) ),
+               Parameters(parameters),
+               Layout(.57, 0.92, 0.92) )
+
+c1.cd(2)
+plot.Draw()
+
+from math import sqrt
+oplus = lambda x,y: sqrt(x*x + y*y)
+
+f1over2 = f1 / f2
+f1over2Err = f1over2 * oplus( f1err / f1, f2err / f2 )
+
+print "%s left/right: %0.4f +/- %0.4f" % ( label, f1over2, f1over2Err )
+
+# c1.Print( 'sFit_clusterCorrection_newOverReco_data_%s.png' % label )
+c1.Print( 'sFit_newClusterCorrection_highOverLowR9_data_%s.png' % label )
+
+
+if __name__ == "__main__": import user
