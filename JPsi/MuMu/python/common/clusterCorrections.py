3ac00b7b7fac56206e1f0daf371d900e5ba5f477
Jan Veverka <jan.veverka@gmail.com>
2011-09-22 18:45:02 +0000

    added JPsi/MuMu/interface to ROOT's include path

--- JPsi/MuMu/python/common/clusterCorrections.py
+++ JPsi/MuMu/python/common/clusterCorrections.py
@@ -5,8 +5,9 @@ import ROOT
 ## Helper variable
 _CMSSW_BASE = os.environ['CMSSW_BASE']
 
-## Add JPsi/MuMu/interface to the include path
-ROOT.gROOT.ProcessLine( '.include %s' % os.path.join( _CMSSW_BASE, 'src' ) )
+## Add JPsi/MuMu/{src,interface} to the include path
+ROOT.gROOT.ProcessLine('.include %s' % os.path.join(_CMSSW_BASE, 'interface'))
+ROOT.gROOT.ProcessLine('.include %s' % os.path.join(_CMSSW_BASE, 'src'))
 
 ## Load compiled cluster correction functions
 ROOT.gROOT.LoadMacro(
