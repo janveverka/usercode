f61f645621a811853fc54a2608060b25e73d5a57
Jan Veverka <jan.veverka@gmail.com>
2011-10-11 17:35:03 +0000

    added missing cmsenv command

--- Misc/Random/scripts/check_fiducial_flags.sh
+++ Misc/Random/scripts/check_fiducial_flags.sh
@@ -4,5 +4,6 @@ cd /tmp/`whoami`
 cd `mktemp -d -p .`
 cmsrel CMSSW_4_2_8
 cd CMSSW_4_2_8/src/
+cmsenv
 cvs co -d Misc/Random UserCode/JanVeverka/Misc/Random
 python -i Misc/Random/test/check_fiducial_flags.py 
