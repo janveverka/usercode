a471b08fc401a74ad4b35f02a62a060aae5cf272
Jan Veverka <jan.veverka@gmail.com>
2011-10-11 17:31:23 +0000

    initial commit

---
+++ Misc/Random/scripts/check_fiducial_flags.sh
@@ -0,0 +1,8 @@
+## Source this file on lxplus5.cern.ch to reproduce plots
+## of EBDetId fiducial flags.
+cd /tmp/`whoami`
+cd `mktemp -d -p .`
+cmsrel CMSSW_4_2_8
+cd CMSSW_4_2_8/src/
+cvs co -d Misc/Random UserCode/JanVeverka/Misc/Random
+python -i Misc/Random/test/check_fiducial_flags.py 
