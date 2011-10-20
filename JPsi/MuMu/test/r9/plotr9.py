ef29dfeead75ae6af1e7d072a22ecc328011772c
Jan Veverka <jan.veverka@gmail.com>
2011-10-20 12:20:19 +0000

    updated to V1 trees

--- JPsi/MuMu/test/r9/plotr9.py
+++ JPsi/MuMu/test/r9/plotr9.py
@@ -15,17 +15,17 @@ path = ("/Users/veverka/Work/Data/r9Trees/SingleGammaFlatPt10To100_"
         "GEN_SIM_DIGI_L1_DIGI2RAW_HLT_RAW2DIGI_L1Reco_noPU_noOOTPU")
 
 filenames = """
-    r9Tree_g93p01.root
-    r9Tree_g94cms.root
-    r9Tree_g94p02.root
+    r9Tree_V1_g93p01.root
+    r9Tree_V1_g94cms.root
+    r9Tree_V1_g94p02.root
     """.split()
 
 files = [ROOT.TFile.Open(os.path.join(path, f)) for f in filenames]
 trees = [f.Get("r9Tree/tree") for f in files]
 
 for i, t in enumerate(trees):
-    t.Draw("photon.r9>>h%d(100,0.5,1.0)" % i,
-           "0.3 < photon.r9 & photon.r9 < 1.1 & abs(photon.eta) > 1.5")
+    t.Draw("r9>>h%d(100,0.5,1.0)" % i,
+           "0.3 < r9 & r9 < 1.1 & abs(eta) > 1.5")
 
 histos = [ROOT.gDirectory.Get("h%d" % i) for i in range(len(trees))]
 
