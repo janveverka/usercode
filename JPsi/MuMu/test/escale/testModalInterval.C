28c2bbdf1f7a90856ba4b44dab85c3fac101943e
Jan Veverka <jan.veverka@gmail.com>
2011-10-26 15:31:04 +0000

    Updted to new method names. Added explanations.

--- JPsi/MuMu/test/escale/testModalInterval.C
+++ JPsi/MuMu/test/escale/testModalInterval.C
@@ -1,14 +1,35 @@
 {
+  /// Configuration
+  double nSigmaCovarage = 1;
+  double mean = 0;
+  double sigma = 1;
+  size_t n = 10000;
+
+  /// Load the needed library and header file.
   gSystem->Load("libJPsiMuMu");
   #include "JPsi/MuMu/interface/ModalInterval.h"
-  size_t n = 10000;
+
+  /// Generate data
+  cout << "Sampling Gaussian PDF with mean=" << mean
+       << " and sigma=" << sigma
+       << " " << n << " times." << endl;
   vector<double> data;
   data.reserve(n);
   for (int i=0; i<n; ++i) {
     data.push_back(gRandom->Gaus(0,1));
   }
-  cit::ModalInterval mi(data.begin(), data.end(), 1);
-  cout << "[" << mi.getLowerBound() << ", " << mi.getUpperBound() << "]" << endl;
-  mi.setFraction(1 - TMath::Prob(pow(0.05,2), 1));
-  cout << "[" << mi.getLowerBound() << ", " << mi.getUpperBound() << "]" << endl;
+
+  /// Construct the modal interval object
+  cit::ModalInterval mi(data.begin(), data.end());
+
+  /// Get the interval to cover all data
+  mi.setFraction(1.);
+  cout << "Shortest interval covering all data: " << endl
+       << "[" << mi.lowerBound() << ", " << mi.upperBound() << "]" << endl;
+
+  /// Get the interval to cover mean +/- nsigma * sigma
+  mi.setSigmaLevel(nSigmaCovarage);
+  cout << "Shortest interval covering same fraction of data as mean +/- "
+       << nSigmaCovarage << " sigma for a Gaussian: " << endl
+       << "[" << mi.lowerBound() << ", " << mi.upperBound() << "]" << endl;
 }
