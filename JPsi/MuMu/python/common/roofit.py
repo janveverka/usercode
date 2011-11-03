8580d2bbe01761e461e1ca4385d47cd241b5e0a7
Jan Veverka <jan.veverka@gmail.com>
2011-11-03 21:23:47 +0000

    Added  Black Magic to Make RooFit work on MacOS X

--- JPsi/MuMu/python/common/roofit.py
+++ JPsi/MuMu/python/common/roofit.py
@@ -4,9 +4,18 @@ import ROOT
 
 _titlePattern = re.compile("^[A-Z]")
 
+### Black Magic to Make RooFit work on MacOS X --------------------------------
+if sys.platform == 'darwin':
+    try:
+        import libRooFit
+    except ImportError:
+        pass
+
+### Define all the callable attributes of ROOT.RooFit
 for method in dir(ROOT.RooFit):
     if callable(getattr(ROOT.RooFit, method)) and re.search(_titlePattern, method):
         if method in vars():
             print "% not imported since it already exists!" % method
         else:
             setattr(sys.modules[__name__], method, getattr(ROOT.RooFit, method))
+
