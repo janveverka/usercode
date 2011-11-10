ba74ae7b8787166e99c1843f9949ca2ef9228123
Jan Veverka <jan.veverka@gmail.com>
2011-11-10 13:20:56 +0000

    Can choose the number of entries

--- JPsi/MuMu/python/common/zeeHggSelYongData.py
+++ JPsi/MuMu/python/common/zeeHggSelYongData.py
@@ -6,13 +6,18 @@ from JPsi.MuMu.common.basicRoot import *
 from JPsi.MuMu.common.zeeHggSelYongChains import getChains
 
 
-def getData(version='v1'):
+def getData(version='v1', nentries=-1):
     chains = getChains(version)
     data = {}
     for name, chain in chains.items():
         mass = RooRealVar('mass', 'mass', 60, 120)
         weight = RooRealVar('weight', 'weight', 0, 999)
-        data[name] = dataset.get(tree=chain, variable=mass, weight=weight)
+        if nentries > 0:
+            cuts = ['Entry$ < %d' % nentries]
+        else:
+            cuts = []
+        data[name] = dataset.get(tree=chain, variable=mass, weight=weight,
+                                 cuts=cuts)
     return data
 
 if __name__ == "__main__":
