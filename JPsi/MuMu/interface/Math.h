e0ea9d0e647cc0dc7930e21c7d3ef1353d6c057d
Jan Veverka <jan.veverka@gmail.com>
2012-01-22 16:39:30 +0000

    Added gshCdf().

--- JPsi/MuMu/interface/Math.h
+++ JPsi/MuMu/interface/Math.h
@@ -10,6 +10,8 @@ namespace cit {
     // Generalized Secant Hyperbolic density with zero mean and unit variance
     void   gsh_a_c1_c2(double t, double &a, double &c1, double &c2);
     double gsh(double x, double t);
+    // CDF of GSH PDF
+    double gshCdf(double x, double t);
   } // end of namespace math
 } // end of namespace cit
 
