4f149c65267d94bc2c83973cdfa8eb607c8be658
Jan Veverka <jan.veverka@gmail.com>
2012-05-08 13:25:13 +0000

    Added functions from interface/tools.h

--- FWLite/Tools/src/LinkDef.h
+++ FWLite/Tools/src/LinkDef.h
@@ -6,6 +6,7 @@
 #include "FWLite/Tools/interface/DataDrivenBinning.h"
 #include "FWLite/Tools/interface/ModalInterval.h"
 #include "FWLite/Tools/interface/RooChi2Calculator.h"
+#include "FWLite/Tools/interface/tools.h"
 
 #ifdef __CINT__
 
@@ -23,6 +24,38 @@
 
 #pragma link C++ global gROOT;
 #pragma link C++ global gEnv;
+
+#pragma link C++ function effSigma(TH1*);
+#pragma link C++ function zMassPdg();
+#pragma link C++ function Oplus(Double_t, Double_t);
+#pragma link C++ function Oplus(Double_t, Double_t, Double_t);
+#pragma link C++ function muonSigmaPtOverPt(Double_t, Double_t);
+#pragma link C++ function photonSigmaEOverE(Double_t, Double_t);
+#pragma link C++ function photonSigmaPtOverPt(Double_t, Double_t);
+#pragma link C++ function phoEtrue(Double_t);
+#pragma link C++ function phoEmeas(Double_t, Double_t);
+#pragma link C++ function kRatio(Double_t, Double_t);
+#pragma link C++ function inverseK(Double_t, Double_t);
+#pragma link C++ function scaledMmgMass10(Double_t, Double_t, Double_t,\
+                                          Double_t, Double_t, Double_t,\
+                                          Double_t, Double_t, Double_t,\
+                                          Double_t);
+#pragma link C++ function scaledMmgMass3(Double_t, Double_t, Double_t);
+#pragma link C++ function scaledDimuonPhotonMass(Double_t, Double_t, Double_t,\
+                                                 Double_t, Double_t, Double_t,\
+                                                 Double_t, Double_t);
+#pragma link C++ function twoBodyMass(Double_t, Double_t, Double_t, Double_t,\
+                                      Double_t, Double_t, Double_t, Double_t);
+#pragma link C++ function threeBodyMass(Double_t, Double_t, Double_t, Double_t,\
+                                        Double_t, Double_t, Double_t, Double_t,\
+                                        Double_t, Double_t, Double_t, Double_t);
+#pragma link C++ function phoSmearE(Double_t, Double_t, Double_t, Double_t,\
+                                    Double_t, Double_t)
+#pragma link C++ function phoSmearF(Double_t, Double_t, Double_t, Double_t,\
+                                    Double_t, Double_t)
+#pragma link C++ function mmgMassPhoSmearE(Double_t, Double_t, Double_t,\
+                                           Double_t, Double_t, Double_t,\
+                                           Double_t, Double_t)
 
 #endif
 #endif
