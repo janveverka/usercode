6e559e41e28bbb88f9b62022817933a932492373
Jan Veverka <jan.veverka@gmail.com>
2012-05-10 13:39:40 +0000

    Can specify model name. Can use exponential background.

--- FWLite/Zee/test/fitBWxCBtoHistForAdi.C
+++ FWLite/Zee/test/fitBWxCBtoHistForAdi.C
@@ -13,13 +13,15 @@
 
 
 void setupModel(RooWorkspace & w);
-void createToyDataHist(RooWorkspace & w, const char *hname, const char *fname);
+/// void createToyDataHist(RooWorkspace & w, const char *hname, const char *fname);
 TH1* getHistFromFile(const char *hname, const char *fname);
 TH1* getHistFromCanvasInFile(const char *hname, const char *cname, 
                              const char *fname);
 void importBinnedData(TH1 *hist, RooWorkspace & w, const char *name="data");
-void fitModelToData(RooWorkspace & w, const char *dataName="data");
-void plotFittedModel(RooWorkspace & w, const char *dataName="data");
+void fitModelToData(RooWorkspace & w, const char *dataName="data",
+                    const char *modelName="model");
+void plotFittedModel(RooWorkspace & w, const char *dataName="data",
+                     const char *modelName="model");
 
 
 ///_____________________________________________________________________________
@@ -27,14 +29,16 @@ void plotFittedModel(RooWorkspace & w, const char *dataName="data");
 void fitBWxCBtoHistForAdi() {
   /// Create a workspace to collect all the relevant objects.
   RooWorkspace w("w", "Fit BWxCB to a histogram");
+  // const char * modelName = "model"; // scb = 2.207
+  const char * modelName = "signal"; // scb = 2.159
 
   setupModel(w);
   // createToyDataHist(w, "toyData", "fitBWxCBHistForAdi.root");
   // TH1 *hist = getHistFromFile("toyData", "fitBWxCBHistForAdi.root");
   TH1 *hist = getHistFromCanvasInFile("dataZmass", "c1", "ZplotData.root");
   importBinnedData(hist, w);
-  fitModelToData(w);
-  plotFittedModel(w);
+  fitModelToData(w, "data", modelName);
+  plotFittedModel(w, "data", modelName);
 } /// fitBWxCBtoHistForAdi()
 
 
@@ -43,15 +47,16 @@ void setupModel(RooWorkspace& w){
   w.factory("BreitWigner::bw(m[30,130], MZ[91.2], GZ[2.5])");
   w.factory("CBShape::cb(m, dm[-10,10], sigma[3, 0.1, 20], "
                         "alpha[1.5, 0.1, 10], power[1.5, 0.1, 10])");
-  w.factory("FCONV::model(m, bw, cb)");
-  w.Print();
+  w.factory("FCONV::signal(m, bw, cb)");
+  w.factory("Exponential::exp(m, decay[-0.04, -100, 100])");
+  w.factory("SUM::model(S[100,0,1e9] * signal, B[10,0,1e9] * exp)");
 } /// setupModel()
 
 
 ///_____________________________________________________________________________
-void createToyDataHist(RooWorkspace &, const char *hname, const char *fname){
-  ;
-} /// createToyDataHist()
+// void createToyDataHist(RooWorkspace &, const char *hname, const char *fname){
+//   ;
+// } /// createToyDataHist()
 
 
 ///_____________________________________________________________________________
@@ -127,18 +132,22 @@ void importBinnedData(TH1 *hist, RooWorkspace & w, const char *name){
 
 
 ///_____________________________________________________________________________
-void fitModelToData(RooWorkspace & w, const char *dataName) {
-  w.pdf("model")->fitTo(*w.data(dataName), RooFit::Range(60, 120));
+void fitModelToData(RooWorkspace & w, const char *dataName, 
+                    const char *modelName) {
+  w.pdf(modelName)->fitTo(*w.data(dataName), RooFit::Range(70, 110));
 } /// fitModelToData()
 
 
 ///_____________________________________________________________________________
-void plotFittedModel(RooWorkspace & w, const char *dataName) {
+void plotFittedModel(RooWorkspace & w, const char *dataName,
+                     const char *modelName) {
   RooPlot * plot = w.var("m")->frame();
   plot->SetTitle(dataName);
   w.data(dataName)->plotOn(plot);
-  w.pdf("model")->plotOn(plot);
-  w.pdf("model")->paramOn(plot);
+  w.pdf(modelName)->plotOn(plot);
+  w.pdf(modelName)->plotOn(plot, RooFit::Components("exp"), 
+                         RooFit::LineStyle(kDashed));
+  w.pdf(modelName)->paramOn(plot);
   TCanvas * canvas = new TCanvas(dataName, dataName);
   plot->Draw();
 } /// plotFittedModel()
