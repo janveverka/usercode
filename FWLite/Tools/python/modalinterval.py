04398fc5e1f92d9317c177defcd9264e79ff08e2
Jan Veverka <jan.veverka@gmail.com>
2012-05-15 13:04:06 +0000

    Removed unnecessary #include directive.

--- FWLite/Tools/python/modalinterval.py
+++ FWLite/Tools/python/modalinterval.py
@@ -1,6 +1,6 @@
 import ROOT
 ROOT.gSystem.Load('libFWLiteTools')
-ROOT.gROOT.ProcessLine('#include "FWLite/Tools/interface/ModalInterval.h"')
+#ROOT.gROOT.ProcessLine('#include "FWLite/Tools/interface/ModalInterval.h"')
 ROOT.gROOT.ProcessLine('typedef vector<double> VDouble')
 # ModalInterval = ROOT.cit.ModalInterval
 ModalInterval = ROOT.ModalInterval
