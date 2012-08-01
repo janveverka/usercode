704ca4e357d69d2d4f29ee4041916f483c64faf2
Jan Veverka <jan.veverka@gmail.com>
2012-08-01 01:52:58 +0000

    Replaced by looseDimuonSequence_cff.py

--- HtoZg/MuonAnalysis/python/preselectedMuonSequence_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from HtoZg.MuonAnalysis.preselectedMuons import *
-
-passMuonPreselections = cms.EDProducer('EventCountProducer')
-preselectedMuonSequence = cms.Sequence(preselectedMuons *
-                                       hasLooseDimuon)
