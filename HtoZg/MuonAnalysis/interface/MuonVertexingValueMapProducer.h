d1b6e4a2652251195176b0a042b31c967a66ce75
Jan Veverka <jan.veverka@gmail.com>
2012-08-03 02:12:52 +0000

    Fleshed out the meat. Needs debugging.

--- HtoZg/MuonAnalysis/interface/MuonVertexingValueMapProducer.h
+++ HtoZg/MuonAnalysis/interface/MuonVertexingValueMapProducer.h
@@ -1,5 +1,3 @@
-#ifndef HtoZg_MuonAnalysis_MuonVertexingValueMapProducer_h
-#define HtoZg_MuonAnalysis_MuonVertexingValueMapProducer_h
 /**
  * \file HtoZg/MuonAnalysis/interface/MuonVertexingValueMapProducer.h
  * \brief Defines Producer of Tight Muon ID Variables that Involve Vertex
@@ -13,6 +11,35 @@
  * \author Jan Veverka, Caltech
  * \date 1 August 2012
  */
+
+#ifndef HtoZg_MuonAnalysis_MuonVertexingValueMapProducer_h
+#define HtoZg_MuonAnalysis_MuonVertexingValueMapProducer_h
+
+// system include files
+#include <memory>
+#include <string>
+#include <vector>
+
+/// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+// #include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "DataFormats/Common/interface/View.h"
+#include "DataFormats/Common/interface/ValueMap.h"
+#include "DataFormats/Common/interface/OwnVector.h"
+
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
 
 namespace cit {
   namespace hzg {
@@ -61,22 +88,35 @@ namespace cit {
     /**
      * Method called for each event
      */
-    template <typename EgammaType>
+    template <typename MuonType>
     void
     MuonVertexingValueMapProducer<MuonType>::produce (
       edm::Event& iEvent, const edm::EventSetup& iSetup
       )
     {
-      using namespace std;
-      using namespace edm;
+      // using namespace std;
+      // using namespace edm;
 
-      Handle<View<MuonType> > muons;
-
-      vector<float> dxy;
-      vector<float> dz;
+      edm::Handle<edm::View<MuonType> > muons;
+      edm::Handle<reco::VertexCollection> vertices; 
       
+      iEvent.getByLabel(muonSource_, muons);
+      iEvent.getByLabel(vertexSource_, vertices);
+
+      std::vector<float> dxy;
+      std::vector<float> dz;
+     
+      reco::Vertex::Point const& vtx = vertices->begin()->position();
+
+      for (MuonView::const_iterator mi = muons->begin(); mi < muons->end();
+           ++mi) {
+        dxy.push_back(mi->dxy(vtx));
+        dz .push_back(mi->dz (vtx));
+      }
+
       putMap(iEvent, muons, dxy, "dxy");
-      putMap(iEvent, muons, dz, "dxy");
+      putMap(iEvent, muons, dz , "dz" );
+
     } /// produce(..)
     
     /**
