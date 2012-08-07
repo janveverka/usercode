3a4c85c0c662d3e8e5181806fd38ccd23bb0c8d4
Jan Veverka <jan.veverka@gmail.com>
2012-08-07 18:10:09 +0000

    Moved manipulation of the PAT sequence near its loading.

--- HtoZg/MuonAnalysis/test/sync/mc_bg_pick_cfg.py
+++ HtoZg/MuonAnalysis/test/sync/mc_bg_pick_cfg.py
@@ -9,15 +9,17 @@ process = copy.deepcopy(HtoZg.CommonAnalysis.process_cfi.process)
 process.source.fileNames.append(
     '/store/mc/Summer12/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/AODSIM/PU_S7_START52_V9-v2/0003/EAF43999-8D9B-E111-A418-003048D4610E.root'
     )
-
+process.source.eventsToProcess = cms.untracked.VEventRange('1:10847674',
+                                                           '1:14658698')
+    
 ## Maximal Number of Events
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
-## Standard PAT Configuration File
-# process.load("PhysicsTools.PatAlgos.patSequences_cff")
-
-## The HtoZg sequence
+## The full skim sequence
 process.load('HtoZg.MuonAnalysis.skimSequence_cff')
+
+## PATification
+#process.load('HtoZg.MuonAnalysis.patSequence_cff')
 patcore.removeAllPATObjectsBut(process, ['Muons'])
 
 ## The muon tree maker
@@ -25,8 +27,11 @@ process.load('HtoZg.MuonAnalysis.muonTree_cfi')
 
 ## TFileService for the ntuple output
 process.load('HtoZg.CommonAnalysis.TFileService_cfi')
+process.TFileService.fileName = 'zg_pick.root'
 
-process.p = cms.Path(process.skimSequence + 
+process.p = cms.Path(
+                     # process.filterSequence +
+                     process.patSequence + 
                      process.muonTree)
 
 if __name__ == '__main__':
