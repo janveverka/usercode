2b7d0529eab7a7f3650fb2b2fb291e3331e67698
Jan Veverka <jan.veverka@gmail.com>
2012-08-07 18:40:02 +0000

    Removed the invariant mass cut.

--- HtoZg/MuonAnalysis/python/looseDimuons_cfi.py
+++ HtoZg/MuonAnalysis/python/looseDimuons_cfi.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 looseDimuons = cms.EDProducer('CandViewShallowCloneCombiner',
     decay = cms.string('looseMuons@+ looseMuons@-'),
     checkCharge = cms.bool(True),
-    cut = cms.string('mass > 50'),
+    cut = cms.string('mass > 0'),
     )
 
