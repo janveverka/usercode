05453a1d3cfa7d382faa596a2dd61d4e395205e4
Jan Veverka <jan.veverka@gmail.com>
2012-08-07 21:50:51 +0000

    Added tree maker after the vertex selection, renamed selectedMuons to tightMuons, and only dump the selected muons after the ID.

--- HtoZg/MuonAnalysis/test/sync/dump.sh
+++ HtoZg/MuonAnalysis/test/sync/dump.sh
@@ -1,7 +1,8 @@
-SOURCE=zg_pick.root
+SOURCE=zg.root
+#SOURCE=zg_pick.root
 root -l -b << EOF
 TFile *_file0 = TFile::Open("$SOURCE")
-muonTree->cd()
+muonsAfterVertexFilter->cd()
 muons->SetScanField(0)
 muons->Scan("id.event:pt:eta:charge:isGlobal:isPF:normChi2:nHit:nMatch:dxy:dz:nPixel:nLayer"); >dump.txt
 .q
--- HtoZg/MuonAnalysis/test/sync/mc_bg_cfg.py
+++ HtoZg/MuonAnalysis/test/sync/mc_bg_cfg.py
@@ -22,12 +22,21 @@ patcore.removeAllPATObjectsBut(process, ['Muons'])
 
 ## The muon tree maker
 process.load('HtoZg.MuonAnalysis.muonTree_cfi')
+process.muonsAfterVertexFilter = process.muonTree.clone()
+process.muonsAfterId = process.muonTree.clone(
+    src = cms.InputTag('tightMuons')
+    )
 
 ## TFileService for the ntuple output
 process.load('HtoZg.CommonAnalysis.TFileService_cfi')
 
-process.p = cms.Path(process.skimSequence + 
-                     process.muonTree)
+process.p = cms.Path(process.allInputEvents +
+                     process.hltFilterSequence + 
+                     process.vertexFilterSequence +
+                     process.patSequence +
+                     process.muonsAfterVertexFilter +
+                     process.tightDimuonSequence +
+                     process.muonsAfterId) 
 
 if __name__ == '__main__':
     ## Adds tab-completion and history for interactive testing.
