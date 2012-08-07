32a211d190b2b13c4a19befb9511d8fe299a2ace
Jan Veverka <jan.veverka@gmail.com>
2012-08-07 18:08:41 +0000

    Renamed output and input files.

--- HtoZg/MuonAnalysis/test/sync/dump.sh
+++ HtoZg/MuonAnalysis/test/sync/dump.sh
@@ -1,9 +1,10 @@
+SOURCE=zg_pick.root
 root -l -b << EOF
-TFile *_file0 = TFile::Open("zg.root")
+TFile *_file0 = TFile::Open("$SOURCE")
 muonTree->cd()
 muons->SetScanField(0)
-muons->Scan("id.event:pt:charge:isGlobal:isPF:normChi2:nHit:nMatch:dxy:dz:nPixel:nLayer"); >Caltech_s12_muons_after_id.txt
+muons->Scan("id.event:pt:eta:charge:isGlobal:isPF:normChi2:nHit:nMatch:dxy:dz:nPixel:nLayer"); >dump.txt
 .q
 EOF
 
-sed -i 's/*//g' Caltech_s12_muons_after_id.txt
+sed -i 's/*//g' dump.txt
