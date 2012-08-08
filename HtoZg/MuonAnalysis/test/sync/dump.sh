68091618334b00350c96509cb4731762cf5da547
Jan Veverka <jan.veverka@gmail.com>
2012-08-08 06:28:21 +0000

    Sync 2012 data

--- HtoZg/MuonAnalysis/test/sync/dump.sh
+++ HtoZg/MuonAnalysis/test/sync/dump.sh
@@ -3,6 +3,7 @@ SOURCE=zg.root
 root -l -b << EOF
 TFile *_file0 = TFile::Open("$SOURCE")
 muonsAfterVertexFilter->cd()
+// muonsAfterId->cd()
 muons->SetScanField(0)
 muons->Scan("id.event:pt:eta:charge:isGlobal:isPF:normChi2:nHit:nMatch:dxy:dz:nPixel:nLayer"); >dump.txt
 .q
