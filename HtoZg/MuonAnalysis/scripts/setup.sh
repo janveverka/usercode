3de3e1d4d0c9c3983711a8645ffa0b6f5c166197
Jan Veverka <jan.veverka@gmail.com>
2012-08-08 07:08:12 +0000

    Initial commit

---
+++ HtoZg/MuonAnalysis/scripts/setup.sh
@@ -0,0 +1,12 @@
+## Recipe to check out and compile the Caltech HtoZg analysis cod
+## Assumes BASH and CMSSW_5_2_x environment.
+## USAGE: source $CMSSW_BASE/src/HtoZg/MuonAnalysis/scripts/setup.sh
+## Jan Veverka, Caltech, 8 August 2012
+
+RTAG=HtoZg_sync_2012_cutbased_muons_uptoId
+cd $CMSSW_BASE/src && \
+    cvs co -r $RTAG -d HtoZg/CommonAnalysis UserCode/JanVeverka/HtoZg/CommonAnalysis && \
+    cvs co -r $RTAG -d HtoZg/MuonAnalysis UserCode/JanVeverka/HtoZg/MuonAnalysis && \
+    cvs co -r $RTAG -d Misc/TreeMaker && \
+    cvs co -D 8/8/12 -d Muon/MuonAnalysisTools UserCode/sixie/Muon/MuonAnalysisTools && \
+    scramv1 build -j4
