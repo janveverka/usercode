9bb3c125d792b7c3cb3175cae7db31caaaf7b028
Jan Veverka <jan.veverka@gmail.com>
2012-08-14 12:01:52 +0000

    Removed the muonIsolation.

--- HtoZg/MuonAnalysis/python/muonUserData_cff.py
+++ HtoZg/MuonAnalysis/python/muonUserData_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 muonUserFloats = [cms.InputTag('muonVertexing', x) for x in 'dxy dz'.split()]
 
-muonUserFloats.extend(
-    [cms.InputTag('muonIsolation', x) for x in 'rho EA combIso'.split()]
-    )
+#muonUserFloats.extend(
+    #[cms.InputTag('muonIsolation', x) for x in 'rho EA combIso'.split()]
+    #)
 
