55c650e042f0ca89c6ef0cc40e0e7c61bd6b1d94
Jan Veverka <jan.veverka@gmail.com>
2012-08-14 11:57:35 +0000

    Added custom rho calculation for the photon PF iso PU subtraction.

--- HtoZg/CommonAnalysis/python/photonUserDataSequence_cff.py
+++ HtoZg/CommonAnalysis/python/photonUserDataSequence_cff.py
@@ -1,7 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 from HtoZg.CommonAnalysis.conversionTools_cfi import conversionTools
+from HtoZg.CommonAnalysis.kt6PFJetsPho_cfi import kt6PFJetsPho
 from HtoZg.CommonAnalysis.photonIsolation_cfi import photonIsolation
 
-photonUserDataSequence = cms.Sequence(conversionTools + photonIsolation)
+photonUserDataSequence = cms.Sequence(
+    conversionTools + 
+    kt6PFJetsPho + 
+    photonIsolation
+    )
 
