8c5e53397034b56a8523bf59485ed0df2c3572c9
Jan Veverka <jan.veverka@gmail.com>
2012-08-14 11:56:38 +0000

    Use custom rho calculated on the fly.

--- HtoZg/CommonAnalysis/python/photonIsolation_cfi.py
+++ HtoZg/CommonAnalysis/python/photonIsolation_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 photonIsolation = cms.EDProducer('RecoPhotonIsolationValueMapProducer',
     photonSource = cms.InputTag('photons'),
-    rhoSource    = cms.InputTag('kt6PFJets', 'rho'),
+    rhoSource    = cms.InputTag('kt6PFJetsPho', 'rho'),
     vertexSource = cms.InputTag('offlinePrimaryVertices'),
     pfSource     = cms.InputTag('particleFlow', '')
     )
