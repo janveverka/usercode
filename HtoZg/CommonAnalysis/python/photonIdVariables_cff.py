eea539b8aa246669f3c901c65a6380119c64aada
Jan Veverka <jan.veverka@gmail.com>
2012-08-14 14:04:27 +0000

    Added passID.

--- HtoZg/CommonAnalysis/python/photonIdVariables_cff.py
+++ HtoZg/CommonAnalysis/python/photonIdVariables_cff.py
@@ -9,6 +9,8 @@ Jan Veverka, Caltech, 11 Aug 2012
 import FWCore.ParameterSet.Config as cms
 import Misc.TreeMaker.tools as tools
 
+from HtoZg.CommonAnalysis.photon_selection import htozg_id
+
 ## Set Loose Cut-Based Photon ID optimized for non-triggering phostons at
 ## https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedPhotonID2012
 ## Revision: r8
@@ -18,4 +20,5 @@ photonIdVariables = tools.get_variables_from_map([
     ('eleVeto', 'userInt("conversionTools:passElectronVeto")'),
     ('hoe'    , 'userFloat("photonId:hadTowOverEm")' ),
     ('sihih'  , 'sigmaIetaIeta'),
+    ('passID', '? %s ? 1 : 0' % htozg_id                      ),
     ])
