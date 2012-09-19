0dda747dd308f148aaa94db6edd890f9340c7f36
Jan Veverka <jan.veverka@gmail.com>
2012-09-19 22:31:39 +0000

    Renamed from VgDimuonSelector for use in the electron channel.

---
+++ Vgamma/Analysis/interface/VgDileptonSelector.h
@@ -0,0 +1,35 @@
+/**
+ * \class VgDileptonSelector
+ * 
+ * \brief Applies the selection to the given dimuon.
+ * 
+ * \author Jan Veverka, Caltech
+ * \date 19 September 2012.
+ */
+#ifndef Vgamma_Analysis_interface_VgDileptonSelector_h
+#define Vgamma_Analysis_interface_VgDileptonSelector_h
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "PhysicsTools/SelectorUtils/interface/Selector.h"
+#include "PhysicsTools/SelectorUtils/interface/strbitset.h"
+#include "Vgamma/Analysis/interface/VgCombinedCandidate.h"
+
+//_____________________________________________________________________
+namespace cit {
+  
+  class VgDileptonSelector : public Selector<VgCombinedCandidate> {
+  public:
+    typedef edm::ParameterSet PSet;
+    /// Ctor and dtor
+    VgDileptonSelector(PSet const &);
+    ~VgDileptonSelector();
+    /// Selection interface
+    using Selector<VgCombinedCandidate>::operator();
+    bool operator()(VgCombinedCandidate const &, pat::strbitset &);
+  private:
+    void init(const double & minMass);
+  }; // class VgDileptonSelector
+  
+} // namespace cit
+
+#endif // #ifndef Vgamma_Analysis_interface_VgDileptonSelector_h
---
+++ Vgamma/Analysis/src/VgDileptonSelector.cc
@@ -0,0 +1,77 @@
+/**
+ * \brief Implementation of the VgDileptonSelector class.
+ * \author Jan Veverka, Caltech
+ * \date 19 September 2012
+ */
+#include <math.h>
+#include <string>
+#include "FWCore/Utilities/interface/Exception.h"
+#include "Vgamma/Analysis/interface/VgAnalyzerTree.h"
+#include "Vgamma/Analysis/interface/VgDileptonSelector.h"
+
+using cit::VgDileptonSelector;
+// using cit::VgEvent;
+using namespace std;
+//_____________________________________________________________________
+/**
+ * Constructor
+ */
+VgDileptonSelector::VgDileptonSelector(PSet const & cfg)
+{
+  init(cfg.getParameter<double>("minMass"));
+
+  if (cfg.exists("cutsToIgnore"))
+    setIgnoredCuts(cfg.getParameter<vector<string> >("cutsToIgnore"));
+
+  retInternal_ = getBitTemplate();  
+} // Ctor
+
+
+//_____________________________________________________________________
+/**
+ * Destructor
+ */
+VgDileptonSelector::~VgDileptonSelector()
+{} // Dtor
+
+
+//_____________________________________________________________________
+/**
+ * Initialization
+ */
+void
+VgDileptonSelector::init(const double & minMass)
+{
+  push_back("Inclusive", 0);
+  push_back("minMass", minMass);
+
+  set("Inclusive");
+  set("minMass");
+} // init()
+
+
+//_____________________________________________________________________
+/**
+ * Selection interface.
+ */
+bool 
+VgDileptonSelector::operator()(cit::VgCombinedCandidate const & ll,
+                             pat::strbitset & ret) 
+{
+  ret.set(false);
+  setIgnored(ret);
+  
+  // 0. all leptons
+  passCut(ret, "Inclusive");
+
+  // 1. dilepton mass is greater than a given minimum
+  if (ll.m() >= cut("minMass", double()) ||
+      ignoreCut("minMass") )
+    passCut(ret, "minMass");
+  else return false;
+
+  return (bool) ret;
+} 
+// VgDileptonSelector::operator()(VgCombinedCandidate const & ll,
+//                              pat::strbitset & ret))
+
