46692982abc93b2cb70ac42fa2b0c8414f477416
Jan Veverka <jan.veverka@gmail.com>
2012-09-23 08:50:52 +0000

    Added histograms of selection variables.

--- Vgamma/Analysis/src/VgMuonHistoFiller.cc
+++ Vgamma/Analysis/src/VgMuonHistoFiller.cc
@@ -7,6 +7,7 @@
 #include <iostream>
 #include "TDirectory.h"
 #include "TMath.h"
+#include "Vgamma/Analysis/interface/VgMuon.h"
 #include "Vgamma/Analysis/interface/VgMuonHistoFiller.h"
 
 using namespace std;
@@ -42,7 +43,26 @@ VgMuonHistoFiller::bookHistograms()
   histos_["muEta"] = new TH1F("muEta", ";#eta_{#mu};Events / 0.1", 
                               60, -3, 3);
   histos_["muPhi"] = new TH1F("muPhi", ";#phi_{#mu};Events / #frac{#pi}{50}", 
-                              100, -TMath::Pi(), TMath::Pi());
+                              102, -1.02 * TMath::Pi(), 1.02 * TMath::Pi());
+  histos_["muIsGlobal"] = new TH1F("muIsGlobal", ";#mu is global;Events", 
+				   4, -1.5 , 2.5);
+  histos_["muNormChi2"] = new TH1F("muNormChi2", ";Muon #chi^{2}/n_{dof};Events / 0.2", 
+				   110, 0, 11);
+  histos_["muChamberHits"] = new TH1F("muChamberHits", ";n^{#mu}_{hits};Events", 
+				      11, -0.5, 10.5);
+  histos_["muStations"] = new TH1F("muStations", ";n^{#mu}_{station};Events", 
+				   6, -0.5, 5.5);
+  histos_["muDxy"] = new TH1F("muDxy", ";d^{#mu}_{xy} (#mum);Events / 10 #mum", 
+			      100, -500, 500);
+  histos_["muDz"] = new TH1F("muDz", ";d^{#mu}_{z} (#mum);Events / 40 #mum", 
+			     100, -2000, 2000);
+  histos_["muPixelHits"] = new TH1F("muPixelHits", ";n^{#mu}_{pixel};Events",
+				    11, -0.5, 10.5);
+  histos_["muTkHits"] = new TH1F("muTkHits", ";n^{#mu}_{tracker};Events", 
+				 51, -0.5, 50.5);
+  histos_["muCombRelIso"] = new TH1F("muCombRelIso", 
+				     ";I^{rel}_{comb} (%);Events / 0.1%", 
+				     110, 0, 11);
 
   cwd->cd();
 
@@ -69,13 +89,25 @@ VgMuonHistoFiller::fillHistograms(cit::VgEvent const& event)
  * Fills the histograms for object with index i.
  */
 void
-VgMuonHistoFiller::fillCand(cit::VgLeafCandidate const & mu)
+VgMuonHistoFiller::fillCand(cit::VgLeafCandidate const & cand)
 {
-  // LeafCand const & mu = dynamic_cast<LeafCand const &>(cand);
-  // unsigned i = mu.key();
+  cit::VgMuon mu(cand);
+  cit::VgAnalyzerTree const & tree = mu.tree();
+  unsigned i = mu.key();
   double wgt = mu.weight();
+
   histos_["muPt" ]->Fill(mu.pt (), wgt);
   histos_["muEta"]->Fill(mu.eta(), wgt);
   histos_["muPhi"]->Fill(mu.phi(), wgt);
+  histos_["muIsGlobal"]->Fill(mu.isGlobalMuon(), wgt);
+  histos_["muNormChi2"]->Fill(tree.muChi2NDF[i], wgt);
+  histos_["muChamberHits"]->Fill(tree.muNumberOfValidMuonHits[i], wgt);
+  histos_["muStations"]->Fill(tree.muStations[i], wgt);
+  histos_["muDxy"]->Fill(1e4 * tree.muPVD0[i], wgt);
+  histos_["muDz"]->Fill(1e4 * tree.muPVDz[i], wgt);
+  histos_["muPixelHits"]->Fill(tree.muNumberOfValidPixelHits[i], wgt);
+  histos_["muTkHits"]->Fill(tree.muNumberOfValidTrkHits[i], wgt);
+  histos_["muCombRelIso"]->Fill(mu.combRelIso(), wgt);
+
 } // VgMuonHistoFiller::fillObjectWithIndex(..)
 
