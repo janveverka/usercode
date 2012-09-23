64c50856a62a946a99aed9c984f17d3bba74debc
Jan Veverka <jan.veverka@gmail.com>
2012-09-23 13:38:52 +0000

    Unused alias to ROOT.RooAbsData.reduce

--- FWLite/Tools/python/roofit.py
+++ FWLite/Tools/python/roofit.py
@@ -14,6 +14,9 @@ if sys.platform == 'darwin':
 ### Rename the RooWorkspace::import method to avoid conflict with Python--------
 setattr(ROOT.RooWorkspace, 'Import', getattr(ROOT.RooWorkspace, 'import'))
 
+### Rename the RooAbsData::reduce method to avoid conflict with Python--------
+# setattr(ROOT.RooAbsData, 'Reduce', getattr(ROOT.RooAbsData, 'reduce'))
+
 ### Define all the callable attributes of ROOT.RooFit
 for method in dir(ROOT.RooFit):
     if callable(getattr(ROOT.RooFit, method)) and re.search(_titlePattern, method):
