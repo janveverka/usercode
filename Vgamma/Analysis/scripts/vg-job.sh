feca651fb7e3c8883574f29a85e1536d4d934312
Jan Veverka <jan.veverka@gmail.com>
2012-09-23 08:51:24 +0000

    Added building of the package.

--- Vgamma/Analysis/scripts/vg-job.sh
+++ Vgamma/Analysis/scripts/vg-job.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 CONFIG=${1:-$CMSSW_BASE/src/Vgamma/Analysis/test/vg_test_cfg.py}
-vg-analyze $CONFIG
-ROOTFILE=$(ls -rt *.root | tail -1)
-rootplot $ROOTFILE --output=${ROOTFILE%*.root}.plots
+scramv1 build -j4 Vgamma/Analysis && \
+    vg-analyze $CONFIG && \
+    ROOTFILE=$(ls -rt *.root | tail -1) && \
+    rootplot $ROOTFILE --output=${ROOTFILE%*.root}.plots
