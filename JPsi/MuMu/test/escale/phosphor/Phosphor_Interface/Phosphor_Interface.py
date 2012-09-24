60b781f6a583a33345bb9a783c94ac8ef91e7089
Cristian Pena <cristian.pena@caltech.edu>
2012-09-24 22:31:32 +0000

    New command line + qsub scripts

--- JPsi/MuMu/test/escale/phosphor/Phosphor_Interface/Phosphor_Interface.py
+++ JPsi/MuMu/test/escale/phosphor/Phosphor_Interface/Phosphor_Interface.py
@@ -35,37 +35,114 @@ def main():
     
     print 'ARGV      :', sys.argv[1:]
     try:
-        options, remainder = getopt.gnu_getopt(sys.argv[1:], 'o:c:vdh', ['cfg_file=','output=','verbose','debug','version=','help',])
+        options, remainder = getopt.gnu_getopt(sys.argv[1:], 'o:c:vdih', ['cfg_file=','output=','verbose','debug','Data=','Det=','treeversion=','R9Low=','R9High=','PtLow=','PtHigh=','help',])
     except getopt.GetoptError, err:
         # print help information and exit:
         print str(err) # will print something like "option -a not recognized"
-        usage()
+        #usage()
         sys.exit(2)
 
-        
+    Globals.CmdFlag = False
+    
     for opt, arg in options:
         #   print 'OPT: ', opt, 'ARG: ', arg
         
         if opt in ('-c', '--cfg_file'):
-            cfg_file = arg    
+            cfg_file = arg
+	    continue
         elif opt in ('-o', '--output'):
             Globals.outputfile = arg
+	    continue
+        elif opt == '-i':
+	    Globals.CmdFlag = True
+	    continue
         elif opt in ('-v', '--verbose'):
             verbose = True
+	    continue
         elif opt in ('-d', '--debug'):
             debug = True
-        elif opt == '--version':
-            version = arg
-        elif opt in ('h','--help'):
+	    continue
+        elif opt == '--treeversion':
+            Globals.model_tree_version = Globals.data_tree_version = arg
+	    continue
+	elif opt == '--Data':
+	    Globals.DataType = arg
+	    continue
+	elif opt == '--Det':
+	    Globals.DetectorType = arg
+            if Globals.DetectorType=='EE':
+                Globals.cuts.append('!phoIsEB')
+ 	        continue
+            elif Globals.DetectorType=='EB':
+                Globals.cuts.append('phoIsEB')
+                continue
+            else:
+                raise RuntimeError, 'Wrong Detector Type, Please Try EE or EB'
+	
+	elif opt == '--PtLow':
+	    Globals.cuts.append('phoPt >= %s' % arg)
+            ptlow = '_PtLow_%s' % arg
+            ptlow1 = arg
+            if debug:
+                print '===CUTS===', Globals.cuts
+            continue
+        elif opt == '--PtHigh':
+            Globals.cuts.append('phoPt < %s' % arg)
+            pthigh = '_PtHigh_%s' % arg
+            pthigh1 = arg
+            if debug:
+                print '===CUTS===', Globals.cuts
+	    continue
+        elif opt == '--R9Low':
+            Globals.cuts.append('phoR9 >= %s' % arg)
+            r9low = '_R9Low_%s' % arg
+            if debug:
+                print '===CUTS===', Globals.cuts
+            continue
+        elif opt == '--R9High':
+	    Globals.cuts.append('phoR9 < %s' % arg)
+            r9high = '_R9high_%s' % arg
+            if debug:
+                print '===CUTS===', Globals.cuts
+            continue
+	elif opt in ('-h','--help'):
             print '========================HERE THE HELP WILL BE PUT!!! HAHA========================================'
             sys.exit(2)
+
     if debug:
         print 'VERSION   :', version
         print 'VERBOSE   :', verbose
         print 'OUTPUT    :', Globals.outputfile
         print 'REMAINING :', remainder
 
-    if cfg_file == 'empty':
+    	
+    if Globals.CmdFlag == True:
+
+        print "********CUTS: ", Globals.cuts
+	Globals.name = Globals.DataType + "_" + Globals.DetectorType + "_" + Globals.model_tree_version + r9low + r9high + ptlow + pthigh #+ dilepcut
+	Globals.latex_title = Globals.DataType + ', ' + Globals.DetectorType + ', E_{T}^{#gamma} [%s,%s]'%(ptlow1 ,pthigh1)
+
+	Globals.cuts.append('mmMass + mmgMass < 180')
+        Globals.cuts.append('minDeltaR < 1.5')
+	Globals.cuts.append('mmgMass > 52')
+        
+        if debug:
+            print 'COMMAND Line CONFIGURATION ------>:'
+            
+        #parse_cfg_file(cfg_file)
+        print "======COMMNAD LINE CONFIGURATION=========", 
+        init_cfg_file()
+        
+        if Globals.DataType == 'data':
+	   print "============= DATA ==========="
+           process_real_data()
+        elif Globals.DataType == 'mc':
+            print "============= MC ==========="
+            process_monte_carlo()
+        else:
+            raise RuntimeError, 'Wrong Data Type, Please Try DataType = data or DataType = mc'
+
+    elif cfg_file == 'empty':
 
         print "USING NAME AS CONFIGURATION FILE"
         
