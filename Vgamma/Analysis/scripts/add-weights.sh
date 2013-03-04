1aeba5abf9495b9323080909f48e4e953ffb4ca7
Jan Veverka <jan.veverka@gmail.com>
2013-03-04 13:04:17 +0000

    initial commit

---
+++ Vgamma/Analysis/scripts/add-weights.sh
@@ -0,0 +1,46 @@
+## Source this in the folder with the inputs to create trees with the proper weights
+
+# add-weight 3048    DYJetsToLL_TuneZ2_M50_Madgraph_Fall11_mmgSkim.root   | sh
+# add-weight 84679.3 QCD_Pt20_MuEnrichedPt15_TuneZ2_Summer11_mmgSkim.root | sh
+# add-weight 165     TTJets_TuneZ2_Madgraph_Fall11_mmgSkim.root           | sh
+# add-weight 31314   WJetsToLNu_TuneZ2_Madgraph_Fall11_mmgSkim.root       | sh
+# add-weight 5.7     WWJetsTo2L2Nu_TuneZ2_Fall11_mmgSkim.root             | sh
+# add-weight 18.2    WZ_TuneZ2_Fall11_mmgSkim.root                        | sh
+# add-weight 12.3    ZGammaMu_2Jet_Madgraph_Fall11_mmgSkim.root           | sh
+# add-weight 5.9     ZZ_TuneZ2_Fall11_mmgSkim.root                        | sh
+# add-weight 12.3    ZgammaMu_1Jet_Madgraph_Fall11_mmgSkim.root           | sh
+
+add-weights 190.56 DiPhotonJets_TuneZ2_Madgraph_Fall11_mmgSkim.root
+add-weights 651.5 GJet_Pt20_DoubleEMEnriched_TuneZ2_Fall11_mmgSkim.root
+add-weights 9614 QCD_Pt30to40_DoubleEMEnriched_TuneZ2_Fall11_mmgSkim.root
+add-weights GJet_Pt15to30_TuneZ2_Summer11_mmgSkim.root
+add-weights GJet_Pt30to50_TuneZ2_Summer11_mmgSkim.root
+add-weights GJet_Pt50to80_TuneZ2_Summer11_mmgSkim.root
+add-weights GJet_Pt80to120_TuneZ2_Summer11_mmgSkim.root
+
+add-weights GJet_Pt300to470_TuneZ2_Summer11_mmgSkim.root
+add-weights GJet_Pt470to800_TuneZ2_Summer11_mmgSkim.root
+
+add-weights QCD_Pt15to30_TuneZ2_Summer11_mmgSkim.root
+
+add-weights QCD_Pt50to80_TuneZ2_Summer11_mmgSkim.root
+add-weights QCD_Pt80to120_TuneZ2_Summer11_mmgSkim.root
+
+add-weights QCD_Pt170to300_TuneZ2_Summer11_mmgSkim.root
+add-weights QCD_Pt300to470_TuneZ2_Summer11_mmgSkim.root
+add-weights QCD_Pt470to600_TuneZ2_Summer11_mmgSkim.root
+
+add-weights 0.444  TTgamma_Madgraph_TuneZ2_Fall11_mmgSkim.root
+add-weights 137.3 WGToENuG_TuneZ2_Fall11_mmgSkim.root
+add-weights 137.3 WGToMuNuG_TuneZ2_Fall11_mmgSkim.root
+add-weights 137.3 WGToTauNuG_TuneZ2_Fall11_mmgSkim.root
+add-weights WW_TuneZ2_Fall11_mmgSkim.root
+add-weights 45.2 ZGToEEG_TuneZ2_Fall11_mmgSkim.root
+add-weights 45.2 ZGToTauTauG_TuneZ2_Fall11_mmgSkim.root
+add-weights WGammaEle_2Jet_Madgraph_Fall11_mmgSkim.root
+add-weights ZGammaEle_2Jet_Madgraph_Fall11_mmgSkim.root
+add-weights WgammaEle_1Jet_Madgraph_Fall11_mmgSkim.root
+add-weights WgammaMu_1Jet_Madgraph_Fall11_mmgSkim.root
+add-weights WgammaTau_1Jet_TuneZ2_Summer11_mmgSkim.root
+add-weights ZgammaEle_1Jet_Madgraph_Fall11_mmgSkim.root
+add-weights ZgammaTau_1Jet_Madgraph_Fall11_mmgSkim.root
