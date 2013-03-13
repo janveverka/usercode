109695bdfbca5f3323b0aebc4168b1dd9f549a35
Jan Veverka <jan.veverka@gmail.com>
2013-03-13 12:36:24 +0000

    beefed up comments

--- FWLite/Tools/python/test/virtual.py
+++ FWLite/Tools/python/test/virtual.py
@@ -1,9 +1,16 @@
 '''
-Test the method virtuality behavior of python methods.
+Test the method virtuality behavior in Python.
 
-Does the method Daughter.name in the derived class Daughter(Mother)
-shadow the method Mother.name even when called within the base
-class in the method Mother.hello?
+Does the method Daughter.name() in the class Daughter(Mother)
+derived from the base class Mother shadow the method 
+Mother.name() even when called within the base
+class in the method Mother.hello()?
+
+Yes.  However, there is a workaround: Mother.hello_workaround().
+You can call the base-class method directly like this:
+Mother.name(self).
+
+USAGE: python virtual.py
 
 Jan Veverka, Caltech, veverka@caltech.edu
 13 March 2013
@@ -30,6 +37,7 @@ class Mother:
         is called.
         '''
         return 'Hello! I am your %s.' % Mother.name(self)
+## End of class Mother
 
 
 #==============================================================================
@@ -40,6 +48,7 @@ class Daughter(Mother):
         Shadows Mother.name().
         '''
         return 'virtual baby'
+## End of class Daughter
 
 
 #==============================================================================
@@ -50,14 +59,16 @@ def main():
     dau = Daughter()
     
     ## Expect one of:
-    ## 1. "Hello! I am your baby." or
-    ## 2. "Hello! I am your mum."
-    ## Dan claims it will be 1.  Will it?
+    ## 1. "Hello! I am your virtual baby." or
+    ## 2. "Hello! I am your real mum."
+    ## Dan claims it will be 1. Will it?
     print dau.hello()
     ## Yes.  However, there is a workaround:
     print dau.hello_workaround()
+## End of main()
 
 
 #==============================================================================
 if __name__ == '__main__':
     main()
+## End of this module
