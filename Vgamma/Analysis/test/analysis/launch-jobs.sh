90115786f91c1970bfe9c749dd00d4b06f608853
Jan Veverka <jan.veverka@gmail.com>
2013-03-27 16:21:41 +0000

    Renamed qcd and zg2jmg5 to qcd20m and zmmg

--- Vgamma/Analysis/test/analysis/launch-jobs.sh
+++ Vgamma/Analysis/test/analysis/launch-jobs.sh
@@ -1,7 +1,9 @@
 ## All jobs
-# JOBS_TO_LAUNCH="data zg2jmg5 zjets ttbar wjets qcd ww wz zz"
+# JOBS_TO_LAUNCH="mm2011AB zmmg zjets ttbar wjets qcd20m ww wz zz"
+
 ## MC only
-# JOBS_TO_LAUNCH="zg2jmg5 zjets ttbar wjets qcd ww wz zz"
+# JOBS_TO_LAUNCH="zmmg zjets ttbar wjets qcd20m ww wz zz"
+
 ## Custom list
 JOBS_TO_LAUNCH="ww wz zz"
 for JOB in $JOBS_TO_LAUNCH; do
