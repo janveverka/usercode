a55fac492cd5a5b38b9b2d8cef4caf3b919cb615
Jan Veverka <jan.veverka@gmail.com>
2013-03-29 09:37:54 +0000

    Switched base class from list to dict

--- Vgamma/Analysis/python/bookkeeping/sampleregister.py
+++ Vgamma/Analysis/python/bookkeeping/sampleregister.py
@@ -3,51 +3,81 @@ Database of samples used in the analysis
 
 Jan Veverka, Caltech, 27 March 2013.
 '''
-
+import imp
+import os
 from Vgamma.Analysis.records.realdatasamplerecord import RealDataSampleRecord
 from Vgamma.Analysis.records.montecarlosamplerecord import MonteCarloSampleRecord
 
+_MODULE_EXTENSIONS = ('.py', '.pyc', '.pyo')
+
 #==============================================================================
-class SampleRegister(list):
+class SampleRegister(dict):
     #__________________________________________________________________________
-    def data(self, *args, **kwargs):
-        '''
-        Registers a data sample.
-        '''
-        self.append(RealDataSampleRecord(*args, **kwargs))
-
-    #__________________________________________________________________________
-    def mc(self, *args, **kwargs):
-        '''
-        Registers an MC sample
-        '''
-        self.append(MonteCarloSampleRecord(*args, **kwargs))
-
-    #__________________________________________________________________________
-    def get(self, name):
-        '''
-        Returns a sample matching the given name.
-        '''
-        match = []
-        for sample in self:
-            if sample.name == name:
-                match.append(sample)
-                
-        if len(match) != 1:
-            message = "Expect 1 sample with name `%s', got %d" % (
-                name, len(match)
-                )
-            raise RuntimeError, message
+    def __init__(self, package):
+        self.package = package
+        self.package_contents = package_contents(package)
+        for name in self.package_contents:
+            record = record_from_module(name, package.__path__)
+            self[record.name] = record
         
-        return match[0]
-    ## End of get
+    ##__________________________________________________________________________
+    #def find(self, name):
+        #'''
+        #Returns a sample matching the given name.
+        #'''
+        #match = []
+        #for sample in self:
+            #if sample.name == name:
+                #match.append(sample)
+                
+        #if len(match) != 1:
+            #message = "Expect 1 sample with name `%s', got %d" % (
+                #name, len(match)
+                #)
+            #raise RuntimeError, message
+        
+        #return match[0]
 
+
+#==============================================================================
+def package_contents(package):
+    '''
+    Returns a set of modules contained in a given package.
+    Example: package_contents('Vgamma/Analysis')
+    >>> set(['Analysis'])
+    Inspired by http://stackoverflow.com/questions/487971/is-there-a-standard-way-to-list-names-of-python-modules-in-a-package
+    '''
+    package_name = package.__name__.replace('.', '/')
+    file, pathname, description = imp.find_module(package_name)
+    if file:
+        raise ImportError('Not a package: %r', package_name)
+    # Use a set because some may be both source and compiled.
+    contents = set([os.path.splitext(module)[0]
+        for module in os.listdir(pathname)
+        if module.endswith(_MODULE_EXTENSIONS)])
+    if '__init__' in contents:
+        contents.remove('__init__')
+        contents = contents.union(set([module
+            for module in os.listdir(pathname)
+            if os.path.isdir(os.path.join(pathname, module))]))
+    return contents
+        
+
+#==============================================================================
+def record_from_module(name, path):
+    '''
+    record_in_path(name, path) -> name from path.name
+    '''
+    found = imp.find_module(name, path)
+    module = imp.load_module(name, *found)
+    return getattr(module, name)
+        
 
 #==============================================================================
 def test():
-    from Vgamma.Analysis.bookkeeping.samples import register
-    print register.get("mm2011AB").__repr__()
-    print register.get("zmmg").__repr__()
+    from Vgamma.Analysis.bookkeeping.samples import mumugamma as register
+    print register["mm2011AB"].__repr__()
+    print register["zmmg"].__repr__()
 
 
 #==============================================================================
