884bedc508d2933f657fb28ed4fb3b1fa42cfa54
Jan Veverka <jan.veverka@gmail.com>
2013-03-29 10:49:52 +0000

    Switched to a separate file per each dataset record.

--- Vgamma/Analysis/python/bookkeeping/samples.py
+++ Vgamma/Analysis/python/bookkeeping/samples.py
@@ -1,62 +1,24 @@
 '''
-Register of samples used in the analysis
+Defines dictionaries of records for samples used in the analysis
 
 Jan Veverka, Caltech, 27 March 2013.
 '''
 
+import Vgamma.Analysis.bookkeeping.march2013mmg as mmg_package
+import Vgamma.Analysis.bookkeeping.march2013eeg as eeg_package
+import Vgamma.Analysis.bookkeeping.test as test_package
 from Vgamma.Analysis.bookkeeping.sampleregister import SampleRegister
 
-register = SampleRegister()
+mumugamma = SampleRegister(mmg_package)
+eegamma = SampleRegister(eeg_package)
+test = SampleRegister(test_package)
 
-## AN-11-251 rev 153517, p. 150
-register.data(
-    name = 'mm2011AB',
-    lumi = 4998.9,
-    )
-
-register.data(
-    name = "ee2011AB", 
-    lumi = 4961.1
-    )
-
-## AN-11-251 rev 153517, p. 7
-register.mc(
-    name = "zmmg", 
-    cross_section = 45.2
-    )
+#______________________________________________________________________________
+def main():
+    print repr(mumugamma['mm2011AB'])
+    print repr(mumugamma['zmmg'])
     
-register.mc(
-    "zjets", 
-    cross_section = 3048.
-    )
-    
-register.mc(
-    "ttbar", 
-    cross_section = 165.
-    )
-    
-register.mc(
-    "wjets", 
-    cross_section = 31314
-    )    
-    
-register.mc(
-    "qcd20m", 
-    cross_section = 84679.3
-    )
-    
-register.mc(
-    "ww", 
-    cross_section = 5.7
-    )
-    
-register.mc(
-    "wz", 
-    cross_section = 18.2
-    )
-    
-register.mc(
-    "zz", 
-    cross_section = 5.9
-    )
-    
+#______________________________________________________________________________
+if __name__ == '__main__':
+    import user
+    main()
