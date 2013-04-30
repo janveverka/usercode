1db2a085c9340904e1b4aed5ce4c686088c16e5a
Jan Veverka <jan.veverka@gmail.com>
2013-04-30 03:13:52 +0000

    Added pkeyspdf_fit

--- JPsi/MuMu/test/escale/thesis/pkeyspdfdemo.py
+++ JPsi/MuMu/test/escale/thesis/pkeyspdfdemo.py
@@ -28,8 +28,10 @@ def main():
     get_saved_data()
     build_model()
     beautify_response_title_and_unit()
-    make_scale_dependence_plot()
-    make_resolution_scan_plot()
+    #make_scale_dependence_plot()
+    #make_resolution_scan_plot()
+    #plot_multiple_models()
+    plot_shape_and_fit()
     canvases.update()
 ## End of main()
 
@@ -64,13 +66,16 @@ def define_workspace_and_variables():
     '''
     Creates the default workspace
     '''
-    global w, mmgMass, mmMass, phoERes, weight, colors, phoScale, phoRes
+    global w, mmgMass, mmMass, phoERes, weight, phoScale, phoRes
+    global colors, linestyles
+    
     w = ROOT.RooWorkspace('w')
 
     ## Define data variables
     mmgMass = w.factory('mmgMass[40, 140]')
     mmMass = w.factory('mmMass[10, 140]')
-    phoERes = w.factory('phoERes[-70, 100]')
+    #phoERes = w.factory('phoERes[-70, 100]')
+    phoERes = w.factory('phoERes[-20, 20]')
     weight = w.factory('weight[1]')
     ## Define model parameters
     phoScale = w.factory('phoScale[0,-20,20]')
@@ -84,6 +89,7 @@ def define_workspace_and_variables():
         ROOT.kMagenta + 2,
         ROOT.kBlack,
         ]
+
 ## End of define_workspace_and_variables()
 
 
@@ -118,7 +124,7 @@ def get_saved_data(sourcename='pkeyspdfdemo.root'):
 ## End of get_saved_data()
   
 ##------------------------------------------------------------------------------
-def build_model():
+def build_model(rho=1.5):
     global phoERes, phoScale, phoRes, data
     global phoEResPdf
     range_save = (phoERes.getMin(), phoERes.getMax())
@@ -127,7 +133,7 @@ def build_model():
     phoERes.setRange(-20, 20)
     phoEResPdf = ParametrizedKeysPdf('phoEResPdf', 'phoEResPdf',
                                      phoERes, phoScale, phoRes, data,
-                                     ROOT.RooKeysPdf.NoMirror, 1.5)
+                                     ROOT.RooKeysPdf.NoMirror, rho)
     phoERes.setRange(*range_save)
 ## End of build_model()
 
@@ -135,7 +141,7 @@ def build_model():
 ##------------------------------------------------------------------------------
 def beautify_response_title_and_unit():
     ## Give the titles the original meaning
-    phoERes.SetTitle('E^{#gamma}/E_{true}^{#gamma} - 1')
+    phoERes.SetTitle('x = E^{#gamma}/E_{true}^{#gamma} - 1')
     phoERes.setUnit('%')
 ## End of beautify_response_title_and_unit()
 
@@ -145,23 +151,25 @@ def make_scale_dependence_plot():
     ## Plot the phoEResPdf for various values of the scale
     canvases.next('ShapeScaleScan').SetGrid()
     phoRes.setVal(1)
-    plot = phoERes.frame(roo.Range(-8, 8))
+    plot = phoERes.frame(roo.Range(-6, 8))
     latexlabels = []
 
     for i, color in enumerate(colors):
         scale = -4 + 2*i
         phoScale.setVal(scale)
         phoEResPdf.plotOn(plot, roo.LineColor(color), roo.Precision(1e-4))
-        label = Latex(['s: %d %%' % scale,],
-                      position=(0.86, 0.75 - (i+1) * 0.055))
+        label = Latex(['s = %d %%' % scale,],
+                      position=(0.8, 0.75 - (i+1) * 0.055),
+                      textsize=24)
         label.SetTextColor(color)
         latexlabels.append(label)
 
-    plot.SetTitle('Dependence on Scale')
-    plot.GetYaxis().SetTitle('Probability Density')
+    # plot.SetTitle('Dependence on Scale')
+    plot.SetTitle('')
+    plot.GetYaxis().SetTitle('Probability Density f(x) (1/%)')
     plot.Draw()
     #Latex(cutlabels, position=(0.2, 0.75)).draw()
-    Latex(['r: 1 %',], position=(0.86, 0.8)).draw()
+    Latex(['r = 1 %',], position=(0.8, 0.8), textsize=24).draw()
     for l in latexlabels:
         l.draw()
 ## Endo fo make_scale_dependence_plot()
@@ -177,8 +185,9 @@ def make_resolution_scan_plot():
         res = 1./(1. + 0.25*(2-i))
         phoRes.setVal(res)
         phoEResPdf.plotOn(plot, roo.LineColor(color), roo.Precision(1e-4))
-        label = Latex(['r: %.2g %%' % res,],
-                      position=(0.75, 0.75 - (i+1) * 0.055))
+        label = Latex(['r = %.2g %%' % res,],
+                      position=(0.75, 0.75 - (i+1) * 0.055),
+                      textsize=24)
         label.SetTextColor(color)
         latexlabels.append(label)
 
@@ -192,49 +201,92 @@ def make_resolution_scan_plot():
         phoRes.setVal(res)
         phoEResPdf.plotOn(plot, roo.LineColor(color))
 
-    plot.SetTitle('Dependence on Resolution')
-    plot.GetYaxis().SetTitle('Probability Density')
+    #plot.SetTitle('Dependence on Scale and Resolution')
+    plot.SetTitle('')
+    plot.GetYaxis().SetTitle('Probability Density f(x) (1/%)')
     plot.Draw()
     # Latex(cutlabels, position=(0.2, 0.75)).draw()
-    Latex(['s: -4, 0, 2 %',], position=(0.75, 0.8)).draw()
+    Latex(['s = -4, 0, 2 %',], position=(0.75, 0.8), textsize=24).draw()
     for l in latexlabels:
         l.draw()
 ## End of make_resolution_scan_plot()
        
+##------------------------------------------------------------------------------
+def plot_multiple_models():
+    linestyles = [
+        ROOT.kDashed,
+        ROOT.kSolid,
+        ROOT.kDotted,
+        #ROOT.kDashDotted,
+        #ROOT.kSolid
+        ]
+    colors = [
+        ROOT.kRed + 1,
+        ROOT.kBlack,
+        ROOT.kBlue + 1,
+        ]
+    build_multiple_models()
+    canvases.next('rho_scan')
+    phoERes.setUnit("%")
+    plot = phoERes.frame(roo.Range(-5, 5))
+    plot.SetTitle("")
+    data.plotOn(plot)
+    for i, model in enumerate(models):
+        model.shape.plotOn(plot, roo.LineColor(colors[i]),
+                           roo.LineStyle(linestyles[i]))
+    plot.Draw()
+## End of plot_multiple_models().
+
 
 ##------------------------------------------------------------------------------
-## Extract the MC truth scale and resolution from MC
-#phoEResPdf.fitTo(data, roo.PrintLevel(-1), roo.SumW2Error(False))
-#phoScaleRef = phoScale.getVal()
-#phoResRef = phoScale.getVal()
+def build_multiple_models(rhos = [1.0, 1.5, 2.0]):
+    global phoEResPdf
+    global models
+    if 'phoEResPdf' in locals():
+        phoEResPdf_backup = phoEResPdf
+    models = []
+    for rho in rhos:
+        build_model(rho)
+        models.append(phoEResPdf)
+    if 'phoEResPdf_backup' in locals():
+        phoEResPdf = phoEResPdf_backup
+## End of build_multiple_models(rhos)
 
-## mmgMassPhoSmearE = w.factory('mmgMassPhoSmearE[40, 140]')
-## phoEResSmear = w.factory('phoEResSmear[-80, 110]')
 
 ##------------------------------------------------------------------------------
-## Plot the nominal MC data overlayed with the pdf shape and fit
-#canvases.next('nominal_fit')
-#phoERes.setUnit("%")
-#plot = phoERes.frame(roo.Range(-7.5, 7.5))
-#plot.SetTitle("MC overlayed with PDF shape (blue) and it's parametrized fit"
-              #"(dashed red)")
-#data.plotOn(plot)
-#phoEResPdf.shape.plotOn(plot)
-#phoEResPdf.plotOn(plot, roo.LineColor(ROOT.kRed), roo.LineStyle(ROOT.kDashed))
-#plot.Draw()
-#Latex([
-    #'s_{shape}: %.3f %%' % phoEResPdf.shapemode,
-    #'s_{fit}: %.3f #pm %.3f %%' % (phoScale.getVal(), phoScale.getError()),
-    #'s_{fit} - s_{shape}: %.4f #pm %.4f %%' % (
-        #phoScale.getVal() - phoEResPdf.shapemode,
-        #phoScale.getError()
-        #),
-    #'r_{shape}: %.3f %%' % phoEResPdf.shapewidth,
-    #'r_{fit}: %.3f #pm %.3f %%' % (phoRes.getVal(), phoRes.getError()),
-    #'r_{fit}/r_{shape}: %.4f #pm %.4f' % (
-        #phoRes.getVal() / phoEResPdf.shapewidth,
-        #phoRes.getError() / phoEResPdf.shapewidth),
-    #], position=(0.2, 0.75)).draw()
+def plot_shape_and_fit():
+    # Extract the MC truth scale and resolution from MC
+    phoEResPdf.fitTo(data, roo.PrintLevel(-1), roo.SumW2Error(False))
+    phoScaleRef = phoScale.getVal()
+    phoResRef = phoScale.getVal()
+
+    # mmgMassPhoSmearE = w.factory('mmgMassPhoSmearE[40, 140]')
+    # phoEResSmear = w.factory('phoEResSmear[-80, 110]')
+
+    #------------------------------------------------------------------------------
+    # Plot the nominal MC data overlayed with the pdf shape and fit
+    canvases.next('pkeyspdf_fit')
+    phoERes.setUnit("%")
+    plot = phoERes.frame(roo.Range(-5, 5))
+    plot.SetTitle('')
+    data.plotOn(plot)
+    phoEResPdf.shape.plotOn(plot)
+    phoEResPdf.plotOn(plot, roo.LineColor(ROOT.kRed), roo.LineStyle(ROOT.kDashed))
+    plot.Draw()
+    Latex([
+        's_{0} = %.3f %%' % phoEResPdf.shapemode,
+        's_{fit} = %.3f #pm %.3f %%' % (phoScale.getVal(), phoScale.getError()),
+        's_{fit} - s_{0} = %.3f #pm %.3f %%' % (
+            phoScale.getVal() - phoEResPdf.shapemode,
+            phoScale.getError()
+            ),
+        'r_{0} = %.3f %%' % phoEResPdf.shapewidth,
+        'r_{fit} = %.3f #pm %.3f %%' % (phoRes.getVal(), phoRes.getError()),
+        'r_{fit}/r_{0} = %.3f #pm %.3f' % (
+            phoRes.getVal() / phoEResPdf.shapewidth,
+            phoRes.getError() / phoEResPdf.shapewidth),
+        ], position=(0.675, 0.82)).draw()
+ ##
 
 ##------------------------------------------------------------------------------
 ## Plot the nominal MC data overlayed with a CB fit
