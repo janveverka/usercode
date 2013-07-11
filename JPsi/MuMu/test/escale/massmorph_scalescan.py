d5962cd859bdc7da1f1720fdde08466494045d2c
Jan Veverka <jan.veverka@gmail.com>
2013-07-11 07:50:20 +0000

    Update

--- JPsi/MuMu/test/escale/massmorph_scalescan.py
+++ JPsi/MuMu/test/escale/massmorph_scalescan.py
@@ -12,10 +12,10 @@ baselinecuts = [
     'phoGenE > 0',
     ]
 
-mm.stargets = [-25 + 2.5*i for i in range(21)]
+mm.stargets = [-25 + 2.5*i for i in range(21)][10:11]
 mm.rtargets = [2.] * len(mm.stargets)
 
-for lo, hi in list(BinEdges([10, 12, 15, 20, 25, 30, 100]))[:]:
+for lo, hi in list(BinEdges([10, 12, 15, 20, 25, 30, 100]))[:1]:
     mm.cuts = baselinecuts + ['%d <= phoPt & phoPt < %d' % (lo, hi)]
     filenamemask = 'massmorph_scalescan_EB_highR9_phoPt%d-%d.root'
     mm.outputfilename = filenamemask % (lo, hi)
@@ -34,7 +34,7 @@ baselinecuts = [
 mm.stargets = [-25 + 2.5*i for i in range(21)]
 mm.rtargets = [2.] * len(mm.stargets)
 
-for lo, hi in list(BinEdges([10, 12, 15, 20, 25, 30, 100]))[:]:
+for lo, hi in list(BinEdges([10, 12, 15, 20, 25, 30, 100]))[:0]:
     mm.cuts = baselinecuts + ['%d <= phoPt & phoPt < %d' % (lo, hi)]
     filenamemask = 'massmorph_scalescan_EB_lowR9_phoPt%d-%d.root'
     mm.outputfilename = filenamemask % (lo, hi)
@@ -53,7 +53,7 @@ baselinecuts = [
 mm.stargets = [-25 + 2.5*i for i in range(21)]
 mm.rtargets = [2.] * len(mm.stargets)
 
-for lo, hi in list(BinEdges([10, 12, 15, 20, 25, 30, 100]))[:]:
+for lo, hi in list(BinEdges([10, 12, 15, 20, 25, 30, 100]))[:0]:
     mm.cuts = baselinecuts + ['%d <= phoPt & phoPt < %d' % (lo, hi)]
     filenamemask = 'massmorph_scalescan_EE_highR9_phoPt%d-%d.root'
     mm.outputfilename = filenamemask % (lo, hi)
@@ -72,7 +72,7 @@ baselinecuts = [
 mm.stargets = [-25 + 2.5*i for i in range(21)]
 mm.rtargets = [2.] * len(mm.stargets)
 
-for lo, hi in list(BinEdges([10, 12, 15, 20, 25, 30, 100]))[:]:
+for lo, hi in list(BinEdges([10, 12, 15, 20, 25, 30, 100]))[:0]:
     mm.cuts = baselinecuts + ['%d <= phoPt & phoPt < %d' % (lo, hi)]
     filenamemask = 'massmorph_scalescan_EE_lowR9_phoPt%d-%d.root'
     mm.outputfilename = filenamemask % (lo, hi)
