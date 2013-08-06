ae84276f85791680e33c78c51d61852671903b9a
Jan Veverka <jan.veverka@gmail.com>
2013-08-06 18:54:12 +0000

    Works on the t3

--- FWLite/Hgg/test/corrections/apply_corrections_cfg.py
+++ FWLite/Hgg/test/corrections/apply_corrections_cfg.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 from copy import copy
 import FWCore.ParameterSet.Config as cms
@@ -7,7 +8,7 @@ process = cms.Process('HggCorrectionsApplication')
 
 photon_pair = lambda variable: ['ph1.' + variable, 'ph2.' + variable]
 
-#______________________________________________________________________________
+#_______________________________________________________________________________
 def correction_pset(raw_variable, corrected_variable,
                     correction_graph_barrel, correction_graph_endcaps):
     '''
@@ -23,7 +24,7 @@ def correction_pset(raw_variable, corrected_variable,
 ## End of correction_pset
 
 
-#______________________________________________________________________________
+#_______________________________________________________________________________
 def correction_pset_pair(raw_variable):
     return [correction_pset('ph1.' + raw_variable,
                             'ph1.' + raw_variable + '_corr',
@@ -36,8 +37,9 @@ def correction_pset_pair(raw_variable):
 ## End of correction_pset_pair
 
 
-#______________________________________________________________________________
+#_______________________________________________________________________________
 process.inputs = cms.PSet(
+    ## Set below
     fileNames = cms.vstring(),
     treeName = cms.string('hPhotonTree'),
     version = cms.string('031'),
@@ -49,8 +51,10 @@ process.inputs = cms.PSet(
     # activeBranches = copy.deepcopy(activeBranchesMC),
     )
 
-## Jan's laptop
-base_path = '/Users/veverka/Work/Data/hgg-2013Final8TeV_ID/'
+### Jan's laptop
+#base_path = '/Users/veverka/Work/Data/hgg-2013Final8TeV_ID/'
+## T3 MIT
+base_path = '/home/veverka/cms/hist/hgg-2013Final8TeV_ID/'
 
 for filename in [
     #'ZGammaToMuMuGamma_2Jet_MG5_TuneZ2_mmgSkim_test10.root',
@@ -61,7 +65,7 @@ for filename in [
         os.path.join(base_path, filename)
         )
 
-#______________________________________________________________________________
+#_______________________________________________________________________________
 output_filename = os.path.join(
     base_path, 'corrected',
     'hgg-2013Final8TeV_ID_s12-zllm50-v7n_skim10k.root'
@@ -79,18 +83,18 @@ process.outputs = cms.PSet(
         )
     )
     
-#______________________________________________________________________________
-process.maxEvents = cms.PSet(
+#_______________________________________________________________________________
+process.maxEntries = cms.PSet(
     toProcess = cms.untracked.int64(-1),
-    reportEvery = cms.untracked.int64(100)
+    reportEvery = cms.untracked.int64(1000)
     )
     
-#______________________________________________________________________________
+#_______________________________________________________________________________
 process.options = cms.PSet(
     verbosity = cms.int64(1)
     )
     
-#______________________________________________________________________________
+#_______________________________________________________________________________
 if __name__ == '__main__':
     import user
     
