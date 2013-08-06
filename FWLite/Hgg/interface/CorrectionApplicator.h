346edc304cc3fbf1acea7165fe30a361aa73118a
Jan Veverka <jan.veverka@gmail.com>
2013-08-06 18:55:16 +0000

    Inits inputs and maxEntries

--- FWLite/Hgg/interface/CorrectionApplicator.h
+++ FWLite/Hgg/interface/CorrectionApplicator.h
@@ -1,60 +1,132 @@
 /**
- * Definition of the CorrectionApplicator class.
- * 
+ * CorrectionApplicator.h - definition of the \class CorrectionApplicator
+ *
  * Facilitates the application of the Q-Q corrections
  * to the photon ID MVA input variables.  Reads a tree
  * on the input and creates a new tree on the output.
  * The new tree contains both raw and corrected variables
  * used as the ID MVA inputs.  It also includes the ID MVA
  * value for both raw and corrected inputs.
- * 
+ *
+ * TODO:
+ *    o Factor out the input chain stuff (config, init) in a separate class
+ *    o Ditto for the output tree
+ *    o Ditto for the BDT?
  * Jan Veverka, MIT, jan.veverka@cern.ch
  * 04 August 2013.
  */
 #ifndef FWLite_Hgg_CorrectionApplicator_h
-#include <boost/shared_ptr.hpp>
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #define FWLite_Hgg_CorrectionApplicator_h
 
+#include <string>
+#include <vector>
+#include <boost/shared_ptr.hpp>
+#include "TChain.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 
-//_____________________________________________________________________
+
+//______________________________________________________________________________
 namespace mit {
   namespace hgg {
     namespace correction_applicator {
+
       /// Forward declarations
       class Configuration;
-      // typedef boost::shared_ptr<Configuration> ConfigPtr;
-      typedef edm::ParameterSet PSet;
-      typedef boost::shared_ptr<PSet> PSetPtr;
+      namespace configuration {
+        class Inputs;
+        class Outputs;
+        class MaxEntries;
+      } // namespace configuration
+      
     } // namespace correction_applicator
 
-    using correction_applicator::Configuration;
-    using correction_applicator::PSetPtr;
-    
-    //_________________________________________________________________
+    //__________________________________________________________________________
     class CorrectionApplicator {
     public:
+      typedef boost::shared_ptr<edm::ParameterSet> PSetPtr;
+      typedef correction_applicator::Configuration Configuration;
+      /// 1st-level decomposition
       CorrectionApplicator(PSetPtr);
       ~CorrectionApplicator();
       void run();
-    protected:
+    private:
+      /// 2nd-level decomposition
       void initialize();
       void beginRun();
-      void loopOverEvents();
+      void loopOverEntries();
       void endRun();
-      Configuration *cfg_;
+      /// 3rd-level decomposition
+      void initializeInputs();
+      void initializeOutputs();
+      void initializeCorrectors();
+      void initializePhotonIdMVAs();
+      void reportEntry(Long64_t ientry, Long64_t entriesToProcess);
+      void processEntry();
+      /// Data members
+      boost::shared_ptr<Configuration> process_;
+      /// the input chain
+      boost::shared_ptr<TChain> ichain_;
     }; // class CorrectionApplicator
 
-    //_______________________________________________________________
+
+    //__________________________________________________________________________
+    /// Provides interface to the configuration
     class correction_applicator::Configuration {
     public:
-      Configuration(PSetPtr cfg) {source_ = cfg;}
+      typedef edm::ParameterSet PSet;
+      typedef boost::shared_ptr<PSet> PSetPtr;
+      typedef boost::shared_ptr<configuration::Inputs> InputsPtr;
+      typedef boost::shared_ptr<configuration::MaxEntries> MaxEntriesPtr;
+      /// 2nd-level decomposition
+      Configuration(PSetPtr source);
       ~Configuration() {}
-      PSetPtr getSource() {return source_;};
+      InputsPtr inputs() const {return inputs_;}
+      MaxEntriesPtr maxEntries() const {return maxEntries_;}
+      // PSetPtr source() {return source_;};
+      // boost_shared
     private:
+      /// 3rd-level decomposition
+      void initialize();
+      /// 4th-level decomposition
+/*      void parseInputs();
+      void parseOutputs();
+      void parsePhotonIdMVA();*/
+      /// Data members
       PSetPtr source_;
+      InputsPtr inputs_;
+      MaxEntriesPtr maxEntries_;
     }; // class correction_applicator::Configuration
+
+
+    //__________________________________________________________________________
+    /// Provides interface to the option section of the configuration.
+    class correction_applicator::configuration::MaxEntries {
+    public:
+      typedef edm::ParameterSet PSet;
+      MaxEntries(Long64_t toProcess, Long64_t reportEvery);
+      void parse(PSet const& cfg);
+      Long64_t toProcess  () const {return toProcess_  ;}
+      Long64_t reportEvery() const {return reportEvery_;}
+    private:
+      Long64_t toProcess_;
+      Long64_t reportEvery_;
+    }; // class correction_applicator::configuration::Option
+
+
+    //__________________________________________________________________________
+    /// Provides interface to the inputs section of the configuration.
+    class correction_applicator::configuration::Inputs {
+    public:
+      typedef edm::ParameterSet PSet;
+      typedef std::vector<std::string> vstring;
+      Inputs(PSet const& cfg);
+      vstring const& fileNames() const {return fileNames_;}
+      std::string const& treeName() const {return treeName_;}
+    private:
+      vstring  fileNames_;
+      std::string treeName_;
+    }; // class correction_applicator::configuration::Inputs
 
   } // namespace hgg
 } // namespace mit
--- FWLite/Hgg/src/CorrectionApplicator.cc
+++ FWLite/Hgg/src/CorrectionApplicator.cc
@@ -6,55 +6,302 @@
  */
 
 #include <iostream>
+#include "FWCore/Utilities/interface/Exception.h"
 #include "FWLite/Hgg/interface/CorrectionApplicator.h"
 
+using namespace std;
 using mit::hgg::CorrectionApplicator;
+using mit::hgg::correction_applicator::Configuration;
+using mit::hgg::correction_applicator::configuration::Inputs;
+using mit::hgg::correction_applicator::configuration::MaxEntries;
 
-//_____________________________________________________________________________
+typedef cms::Exception Bad;
+
+//______________________________________________________________________________
 /**
  * Constructor
  */
 CorrectionApplicator::CorrectionApplicator(PSetPtr cfg) :
-  cfg_(new Configuration(cfg))
+  process_(new Configuration(cfg)),
+  ichain_(new TChain("CorrectionApplicatorInputChain"))
 {
   initialize();
 } // Constructor
 
 
-//_____________________________________________________________________________
+//______________________________________________________________________________
 /**
  * Destructor
  */
 CorrectionApplicator::~CorrectionApplicator()
 {
-  delete cfg_;
 } // Destructor
 
 
-//_____________________________________________________________________________
+//______________________________________________________________________________
 /**
  * Run the correction application.
  */
 void
 CorrectionApplicator::run()
 {
-  std::cout << "CorrectionApplicator::run()\n";
 //   beginRun();
-//   loopOverEvents();
+  loopOverEntries();
 //   endRun();
 } // run()
 
 
-//_____________________________________________________________________________
+
+
+//==============================================================================
+// 2nd  L E V E L   D E C O M P O S I T I O N
+//==============================================================================
+
+
+//___________________________________________________________________________
+/**
+ * Loops over the entries.
+ */
+void
+CorrectionApplicator::loopOverEntries()
+{
+  Long64_t entriesToProcess = ichain_->GetEntries();
+  Long64_t maxEntries = process_->maxEntries()->toProcess();
+  
+  if (maxEntries > 0 && maxEntries < entriesToProcess) {
+    entriesToProcess = maxEntries;
+  }
+  
+  for (Long64_t ientry = 0; ientry < entriesToProcess; ++ientry) {
+    if (ichain_->LoadTree(ientry) < 0) break;
+    reportEntry(ientry, entriesToProcess);
+    processEntry();
+  }
+} // CorrectionApplicator::loopOverEntries()
+
+
+//==============================================================================
+// 3rd  L E V E L   D E C O M P O S I T I O N
+//==============================================================================
+
+
+//______________________________________________________________________________
 /**
  * Initializes data members.
- * Parses configuration, opens input file, retrieves the input tree,
- * defines variables to be read, sets their addresses, creates the 
+ * Parses configuration (in cfg ctor), opens input file, retrieves the input
+ * tree, defines variables to be read, sets their addresses, creates the
  * output file and tree, initializes corrections.
+ * Pre-conditions: Valid configuration exists.
+ * Post-conditions: Input, output and corrector fields are initialized.
  */
 void
 CorrectionApplicator::initialize()
 {
-} // initialize()
+  initializeInputs();
+  initializeOutputs();
+  initializeCorrectors();
+  initializePhotonIdMVAs();
+} // CorrectionApplicator::initialize()
+
+
+//______________________________________________________________________________
+/**
+ * Initializes the input file and tree.
+ * Pre-conditions: Valid configuration exists.
+ * Post-conditions: The input tree and branch buffers are initialized and
+ *                  the address are set.
+ */
+void
+CorrectionApplicator::initializeInputs()
+{
+  ichain_->SetName(process_->inputs()->treeName().c_str());
+
+  Inputs::vstring const& fileNames = process_->inputs()->fileNames();
+  for (Inputs::vstring::const_iterator fileName = fileNames.begin();
+       fileName != fileNames.end(); ++fileName)
+  {
+    ichain_->Add(fileName->c_str());
+  }
+
+} // CorrectionApplicator::initializeInputs()
+
+
+//______________________________________________________________________________
+/**
+ * Initializes
+ */
+void
+CorrectionApplicator::initializeOutputs()
+{
+} // CorrectionApplicator::initializeOutputs()
+
+
+//______________________________________________________________________________
+/**
+ *
+ */
+void
+CorrectionApplicator::initializeCorrectors()
+{
+} // CorrectionApplicator::initializeCorrectors()
+
+
+//______________________________________________________________________________
+/**
+ *
+ */
+void
+CorrectionApplicator::initializePhotonIdMVAs()
+{
+} // CorrectionApplicator::initializePhotonIdMVAs()
+
+
+//___________________________________________________________________________
+/**
+ * Processes the current entry.
+ */
+void
+CorrectionApplicator::processEntry()
+{
+} // CorrectionApplicator::processEntry()
+
+
+//___________________________________________________________________________
+/**
+ * Reports the current entry.
+ */
+void
+CorrectionApplicator::reportEntry(Long64_t ientry, Long64_t entriesToProcess)
+{
+  if (ientry % process_->maxEntries()->reportEvery() == 0) {
+    std::cout << "Processing entry " << ientry << std::endl;
+  }
+} // CorrectionApplicator::reportEntry()
+
+
+// Method boiler plate
+// //___________________________________________________________________________
+// /**
+//  *
+//  */
+// void
+// CorrectionApplicator::()
+// {
+// } // CorrectionApplicator::()
+
+
+
+
+//==============================================================================
+/// Implementation of \class Configuration
+//==============================================================================
+
+
+//______________________________________________________________________________
+/**
+ * Configuration Ctor
+ */
+Configuration::Configuration(PSetPtr source) :
+  source_(source),
+  inputs_(new Inputs(source->getParameter<PSet>("inputs"))),
+  maxEntries_(new MaxEntries(/* toProcess   default */ -1,
+                             /* reportEvery default */  1))
+{
+  initialize();
+} // Configuration::Configuration()
+
+
+//___________________________________________________________________________
+/**
+ * Initialize configuration
+ */
+void
+Configuration::initialize()
+{
+  if (source_->existsAs<PSet>("maxEntries")) {
+    maxEntries_->parse(source_->getParameter<PSet>("maxEntries"));
+  }
+} // Configuration::initialize()
+
+
+// Method boiler plate
+// //___________________________________________________________________________
+// /**
+//  *
+//  */
+// void
+// Configuration::()
+// {
+// } // Configuration::()
+
+
+
+
+//==============================================================================
+/// Implementation of \class Inputs
+//==============================================================================
+
+
+//___________________________________________________________________________
+/**
+ * Ctor
+ */
+Inputs::Inputs(PSet const& cfg) :
+  fileNames_(cfg.getParameter<vstring>("fileNames")),
+  treeName_(cfg.getParameter<std::string>("treeName"))
+{
+} // Inputs::Inputs()
+
+
+// //___________________________________________________________________________
+// /**
+//  *
+//  */
+// void
+// Inputs::()
+// {
+// } // Inputs::()
+
+
+
+
+//==============================================================================
+/// Implementation of \class MaxEntries
+//==============================================================================
+
+
+//___________________________________________________________________________
+/**
+ * Ctor
+ */
+MaxEntries::MaxEntries(Long64_t toProcess, Long64_t reportEvery) :
+  toProcess_  (toProcess  ),
+  reportEvery_(reportEvery)
+{} // MaxEntries::MaxEntries()
+
+
+//___________________________________________________________________________
+/**
+ * Parses the given parameter set and updates the values of entries to
+ * process and to report.
+ */
+void
+MaxEntries::parse(PSet const& cfg)
+{
+  toProcess_   = cfg.getUntrackedParameter<Long64_t>("toProcess"  ,
+                                                      toProcess_  );
+  reportEvery_ = cfg.getUntrackedParameter<Long64_t>("reportEvery",
+                                                      reportEvery_);
+} // MaxEntries::parse()
+
+
+// //___________________________________________________________________________
+// /**
+//  *
+//  */
+// void
+// MaxEntries::()
+// {
+// } // MaxEntries::()
 
 
