1c90d9338d3a096e8e5f0614c333e831191ab9aa
Jan Veverka <jan.veverka@gmail.com>
2013-09-25 19:32:09 +0000

    Initial commit

---
+++ FWLite/Tools/bin/hello.cc
@@ -0,0 +1,15 @@
+/**
+ * Example a source of an executable.
+ * Prints "hello, world!" to the standard output.  
+ * 
+ * Jan Veverka, MIT, 25 September 2013
+ */
+
+#include <iostream>
+
+//_____________________________________________________________________________
+int main(int argc, char **argv) { 
+  std::cout << "hello, world!" << std::endl;
+  return 0;
+} 
+
